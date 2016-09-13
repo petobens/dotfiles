6792839ba4b2e8422d845ba9c9ec0c56ef3497d2
petobens <ferrari_pedro@yahoo.com>
2016-09-12 23:25:54 -0300

    Only symlink hammerspon and arara on mac.

--- symlinks.sh
+++ symlinks.sh
@@ -10,7 +10,6 @@
 # FIXME: symbolic links to directories replicate target directory
 dotfiles_dir="$HOME/git-repos/private/dotfiles"
 ln -s "$dotfiles_dir/ctags" "$HOME/.ctags"
-ln -s "$dotfiles_dir/hammerspoon" "$HOME/.hammerspoon"
 ln -s "$dotfiles_dir/pentadactyl" "$HOME/.pentadactyl"
 ln -s "$dotfiles_dir/pentadactylrc" "$HOME/.pentadactylrc"
 ln -s "$dotfiles_dir/gitignore" "$HOME/.gitignore"
@@ -20,13 +19,14 @@ ln -s "$dotfiles_dir/vimrc" "$HOME/.vimrc"
 ln -s "$dotfiles_dir/vim/" "$HOME/.config/nvim"
 ln -s "$dotfiles_dir/vimrc" "$HOME/.config/nvim/init.vim"
 ln -s "$dotfiles_dir/vintrc.yaml" "$HOME/.vintrc.yaml"
-ln -s "$dotfiles_dir/arararc.yaml" "$HOME/.arararc.yaml"
 ln -s "$dotfiles_dir/Rprofile" "$HOME/.Rprofile"
 ln -s "$dotfiles_dir/bash_profile" "$HOME/.bash_profile"
 ln -s "$dotfiles_dir/config/powerline" "$HOME/.config/powerline"
 
 if [[ "$OSTYPE" == 'darwin'* ]]; then
     ln -s "$dotfiles_dir/gitconfig_mac" "$HOME/.gitconfig"
+    ln -s "$dotfiles_dir/hammerspoon" "$HOME/.hammerspoon"
+    ln -s "$dotfiles_dir/arararc.yaml" "$HOME/.arararc.yaml"
 else
     ln -s "$dotfiles_dir/gitconfig_linux" "$HOME/.gitconfig"
 fi
