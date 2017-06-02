271f784f9d037349870b3944d5a56882a58de287
petobens <ferrari_pedro@yahoo.com>
2017-06-02 16:26:25 -0300

    Add yarn

--- npm.sh
+++ npm.sh
@@ -3,9 +3,11 @@
 #          File: npm.sh
 #        Author: Pedro Ferrari
 #       Created: 26 May 2017
-# Last Modified: 26 May 2017
+# Last Modified: 02 Jun 2017
 #   Description: npm libraries
 #===============================================================================
 npm install -g eslint
 npm install -g jsonlint
 npm install -g tern
+npm install -g yarn # npm on steroids
+
