5f322dbd84702b2c4425e1486b4f584f1577234e
petobens <ferrari_pedro@yahoo.com>
2018-08-07 19:23:32 -0300

    Better dunst colors and open apps in specific workspace

--- arch/i3/spotify_track.py
+++ arch/i3/spotify_track.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
-import json
-
 from subprocess import Popen
 
 import gi
 
 gi.require_version('Playerctl', '1.0')
 
-from gi.repository import GLib, Playerctl
+from gi.repository import Playerctl
 
 player = Playerctl.Player()
 title = player.get_title()
 artist = player.get_artist()
 album = player.get_album()
 
-Popen(
-    [
-        'dunstify', '-a', 'spotify', title, f'{artist}\n{album}', '-i',
-        'Spotify'
-    ]
-)
+Popen(['dunstify', '-a', 'Spotify', title, f'{artist}\n{album}'])
