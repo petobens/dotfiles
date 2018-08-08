33336c2b36be7ed70ada17f7d3bf65f37dd86b45
petobens <ferrari_pedro@yahoo.com>
2018-08-07 21:31:05 -0300

    Better rofi config (including application switcher)

---
+++ arch/i3/tab_switcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rofi \
+    -show window  \
+    -no-fixed-num-lines \
+    -kb-accept-entry "!Alt-Tab,Return"\
+    -kb-row-down "Alt+Tab,Ctrl-n" \
+    -kb-row-up "Shift_L+Left_Tab,Ctrl-p"&
+xdotool keyup Tab
+xdotool keydown Tab
