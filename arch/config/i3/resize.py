71ced1e2a504030b11c4145559485e0800b1f5dd
petobens <ferrari_pedro@yahoo.com>
2018-08-12 16:17:59 -0300

    i3: Add floating window resizer script

---
+++ arch/config/i3/resize.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import sys
+
+import i3ipc
+
+
+def resize_win(x, y, w, h):
+    win = i3.get_tree().find_focused()
+    ws = win.workspace()
+    max_h = ws.rect.height
+    max_w = ws.rect.width
+    max_x = ws.rect.x
+    max_y = ws.rect.y
+
+    x = int(max_x + (max_w * x))
+    y = int(max_y + (max_h * y))
+    w = int(max_w * w)
+    h = int(max_h * h)
+    cmd = f"fullscreen disable, floating enable, " \
+          f"move position {x} {y}, resize set {w} {h}"
+    i3.command(cmd)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('how')
+    args = parser.parse_args()
+    how = args.how
+
+    i3 = i3ipc.Connection()
+    if how == 'Full':
+        resize_win(0, 0, 1, 1)
+    elif how == 'Left':
+        resize_win(0, 0, 0.5, 1)
+    elif how == 'Right':
+        resize_win(0.5, 0, 0.5, 1)
+    elif how == 'Top':
+        resize_win(0, 0, 1, 0.5)
+    elif how == 'Bottom':
+        resize_win(0, 0.5, 1, 0.5)
+    elif how == 'Top Left':
+        resize_win(0, 0, 0.5, 0.5)
+    elif how == 'Top Right':
+        resize_win(0.5, 0, 0.5, 0.5)
+    elif how == 'Bottom Left':
+        resize_win(0, 0.5, 0.5, 0.5)
+    elif how == 'Bottom Right':
+        resize_win(0.5, 0.5, 0.5, 0.5)
+    elif how == 'Center':
+        resize_win(0.25, 0.25, 0.5, 0.5)
+    sys.exit(0)
