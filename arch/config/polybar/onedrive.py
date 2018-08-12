abd040ae780742b339886fd9471e2d1076bafc7a
petobens <ferrari_pedro@yahoo.com>
2018-08-12 12:03:38 -0300

    polybar: clean onedrive script a bit

--- arch/config/polybar/onedrive.py
+++ arch/config/polybar/onedrive.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
 import re
 import subprocess
 
 from pathlib import Path
 
+status = None
 LOCATIONS = [Path.home(), '/var/lib/onedrive']
 FILE_NAME = 'onedrive.log'
-ACTIVE = ['Downloading', 'Creating', 'Syncing']
-status = None
-
 for l in LOCATIONS:
     log_file = Path(f"{l}/{FILE_NAME}")
     if log_file.is_file():
@@ -19,14 +16,19 @@ else:
 last_line = subprocess.check_output(
     ['tail', '-1', log_file], universal_newlines=True
 )
-
 m = re.search('\.\d+\s(\w*)\s', last_line)
 if m:
     status = m.groups()[0]
 else:
     pass
 
-if status in ACTIVE:
+if status in ['Downloading']:
+    print(" ")
+elif status in ['Uploading']:
+    print(" ")
+elif status in ['Creating']:
     print(" ")
+elif status in ['Syncing']:
+    print("")
 else:
-    print(" 裏")
+    print("%{F#e06c75}{F-}")
