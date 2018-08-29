8ccf15d8b7c4c0f5d9d9b9759d8f4e12312d0bdb
petobens <ferrari_pedro@yahoo.com>
2018-08-29 11:54:21 -0300

    setup: refactor symlinks file

--- setup-new/symlinks.sh
+++ setup-new/symlinks.sh
@@ -3,10 +3,8 @@
 # Check bash major version
 bash_version=${BASH_VERSION:0:1}
 
-# Ask for dotfiles dir. Note: the -i flag is only available on Bash 4
-cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# The actual dotfiles dir is the directory above the current one
-cur_dir="$(dirname "$cur_dir")"
+# Ask for dotfiles dir (the -i flag is only available on Bash 4)
+cur_dir="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
 if [[ $bash_version -gt 3 ]]; then
     read -r -e -p "Enter dotfiles directory: " -i "$cur_dir" dotfiles_dir
 else
@@ -20,222 +18,190 @@ while [ ! -d "$dotfiles_dir" ]; do
         read -r -e -p "Enter dotfiles directory: " dotfiles_dir
     fi
 done
+dotfiles_dir=${dotfiles_dir%/}   # Strip last (potential) slash
 
-# Strip last (potential) slash
-dotfiles_dir=${dotfiles_dir%/}
-
+# Creating missing dirs
 echo Creating symlinks under "$HOME"/
+mkdir -p "$HOME/.config/"
 
 # First symlink bashrc and reload it without logging out and back in
 if type "bash" > /dev/null 2>&1; then
-    rm -rf "$HOME/.bashrc"
-    ln -s "$dotfiles_dir/bashrc" "$HOME/.bashrc"
+    ln -fTs "$dotfiles_dir/bashrc" "$HOME/.bashrc"
     echo Created .bashrc symlink
-    rm -rf "$HOME/.bash_profile"
-    ln -s "$dotfiles_dir/bash_profile" "$HOME/.bash_profile"
+    ln -fTs "$dotfiles_dir/bash_profile" "$HOME/.bash_profile"
     echo Created .bash_profile symlink
 fi
 . "$HOME/.bashrc"
 
-if type "ctags" > /dev/null 2>&1; then
-    rm -rf "$HOME/.ctags"
-    ln -s "$dotfiles_dir/ctags" "$HOME/.ctags"
-    echo Created .ctags symlink
-fi
+# Language related
 if type "python" > /dev/null 2>&1; then
-    rm -rf "$HOME/.pdbrc"
-    ln -s "$dotfiles_dir/python/pdbrc" "$HOME/.pdbrc"
+    ln -fTs "$dotfiles_dir/python/pdbrc" "$HOME/.pdbrc"
     echo Created .pdbrc symlink
+    if type "pip" > /dev/null 2>&1; then
+        ln -fTs "$dotfiles_dir/config/pip" "$HOME/.config/pip"
+        echo Created .config/pip folder symlink
+    fi
 fi
 if type "ipython" > /dev/null 2>&1; then
     mkdir -p "$HOME/.ipython/profile_default/startup"
-    rm -rf "$HOME/.ipython/profile_default/ipython_config.py"
-    ln -s "$dotfiles_dir/python/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
-    echo Created .ipython_config symlink
-    rm -rf "$HOME/.ipython/profile_default/startup/ipython_startup.py"
-    ln -s "$dotfiles_dir/python/ipython_startup.py" "$HOME/.ipython/profile_default/startup/ipython_startup.py"
-    echo Created .ipython_startup symlink
+    ln -fTs "$dotfiles_dir/python/ipython_config.py" "$HOME/.ipython/profile_default/ipython_config.py"
+    echo Created .ipython/profile_default/ipython_config symlink
+    ln -fTs "$dotfiles_dir/python/ipython_startup.py" "$HOME/.ipython/profile_default/startup/ipython_startup.py"
+    echo Created .ipython/profile_default/startup/ipython_startup symlink
+fi
+if type "R" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/R/Rprofile" "$HOME/.Rprofile"
+    echo Created .Rprofile symlink
+fi
+
+# Coding environment
+if type "alacritty" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/config/alacritty" "$HOME/.config/alacritty"
+    echo Created .config/alacritty folder symlink
 fi
 if type "tmux" > /dev/null 2>&1; then
-    rm -rf "$HOME/.tmux"
-    ln -s "$dotfiles_dir/tmux" "$HOME/.tmux"
+    ln -fTs "$dotfiles_dir/tmux" "$HOME/.tmux"
     echo Created .tmux folder symlink
 fi
-if type "nvim" > /dev/null 2>&1; then
-    rm -rf "$HOME/.vim"
-    ln -s "$dotfiles_dir/vim/" "$HOME/.vim"
-    echo Created .vim folder symlink
-    rm -rf "$HOME/.vimrc"
-    ln -s "$dotfiles_dir/vimrc" "$HOME/.vimrc"
-    echo Created .vimrc symlink
-    mkdir -p "$HOME/.config/"
-    rm -rf "$HOME/.config/nvim"
-    ln -s "$dotfiles_dir/vim" "$HOME/.config/nvim"
-    echo Created ./config/nvim folder symlink
-    rm -rf "$HOME/.config/nvim/init.vim"
-    ln -s "$dotfiles_dir/vimrc" "$HOME/.config/nvim/init.vim"
-    echo Created .init.vim symlink
+if type "powerline-daemon" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/config/powerline" "$HOME/.config/powerline"
+    echo Created .config/powerline folder symlink
 fi
+if type "vim" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/vim" "$HOME/.vim"
+    echo Created .vim folder symlink
+    ln -fTs "$dotfiles_dir/vimrc" "$HOME/.vimrc"
+    echo Created .vimrc symlink
+fi
+if type "nvim" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/vim" "$HOME/.config/nvim"
+    echo Created .config/nvim folder symlink
+    ln -fTs "$dotfiles_dir/vimrc" "$HOME/.config/nvim/init.vim"
+    echo Created .config/nvim/.init.vim symlink
+fi
+if type "ctags" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/ctags" "$HOME/.ctags"
+    echo Created .ctags symlink
+fi
+# Browser
+ln -fTs "$dotfiles_dir/surfingkeysrc.js" "$HOME/.surfingkeysrc"
+echo Created .surfingkeysrc symlink
+
+# Linters
 if type "vint" > /dev/null 2>&1; then
-    rm -rf "$HOME/.vintrc.yaml"
-    ln -s "$dotfiles_dir/linters/vintrc.yaml" "$HOME/.vintrc.yaml"
+    ln -fTs "$dotfiles_dir/linters/vintrc.yaml" "$HOME/.vintrc.yaml"
     echo Created .vintrc.yaml symlink
 fi
 if type "eslint" > /dev/null 2>&1; then
-    rm -rf "$HOME/.eslintrc.yaml"
-    ln -s "$dotfiles_dir/linters/eslintrc.yaml" "$HOME/.eslintrc.yaml"
+    ln -fTs "$dotfiles_dir/linters/eslintrc.yaml" "$HOME/.eslintrc.yaml"
     echo Created .eslintrc.yaml symlink
 fi
-if type "R" > /dev/null 2>&1; then
-    rm -rf "$HOME/.Rprofile"
-    ln -s "$dotfiles_dir/R/Rprofile" "$HOME/.Rprofile"
-    echo Created .Rprofile symlink
-fi
-if type "powerline-daemon" > /dev/null 2>&1; then
-    rm -rf  "$HOME/.config/powerline"
-    ln -s "$dotfiles_dir/config/powerline" "$HOME/.config/powerline"
-    echo Created .config/powerline folder symlink
-fi
-if type "arara" > /dev/null 2>&1; then
-    rm -rf "$HOME/.arararc.yaml"
-    ln -s "$dotfiles_dir/arararc.yaml" "$HOME/.arararc.yaml"
-    echo Created .arararc.yaml symlink
-fi
-if type "mutt" > /dev/null 2>&1; then
-    rm -rf "$HOME/.config/mutt"
-    ln -s "$dotfiles_dir/config/mutt" "$HOME/.config/mutt"
-    echo Created .config/mutt folder symlink
-fi
-if type "ranger" > /dev/null 2>&1; then
-    rm -rf "$HOME/.config/ranger"
-    ln -s "$dotfiles_dir/config/ranger" "$HOME/.config/ranger"
-    echo Created .config/ranger folder symlink
-fi
 if type "tern" > /dev/null 2>&1; then
-    rm -rf "$HOME/.tern-config"
-    ln -s "$dotfiles_dir/linters/tern-config" "$HOME/.tern-config"
+    ln -fTs "$dotfiles_dir/linters/tern-config" "$HOME/.tern-config"
     echo Created .tern-config symlink
 fi
 if type "htmlhint" > /dev/null 2>&1; then
-    rm -rf "$HOME/.htmlhintrc"
-    ln -s "$dotfiles_dir/linters/htmlhintrc" "$HOME/.htmlhintrc"
+    ln -fTs "$dotfiles_dir/linters/htmlhintrc" "$HOME/.htmlhintrc"
     echo Created .htmlhintrc symlink
 fi
-if type "pip" > /dev/null 2>&1; then
-    rm -rf "$HOME/.config/pip"
-    ln -s "$dotfiles_dir/config/pip" "$HOME/.config/pip"
-    echo Created .config/pip folder symlink
+
+# Terminal programs
+if type "arara" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/arararc.yaml" "$HOME/.arararc.yaml"
+    echo Created .arararc.yaml symlink
 fi
-
-rm -rf "$HOME/.surfingkeysrc"
-ln -s "$dotfiles_dir/surfingkeysrc.js" "$HOME/.surfingkeysrc"
-echo Created .surfingkeysrc symlink
-
-rm -rf "$HOME/.config/alacritty"
-ln -s "$dotfiles_dir/config/alacritty" "$HOME/.config/alacritty"
-echo Created .config/alacritty folder symlink
+if type "mutt" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/config/mutt" "$HOME/.config/mutt"
+    echo Created .config/mutt folder symlink
+fi
+if type "ranger" > /dev/null 2>&1; then
+    ln -fTs "$dotfiles_dir/config/ranger" "$HOME/.config/ranger"
+    echo Created .config/ranger folder symlink
+fi
 
 # OS dependent
 if [[ "$OSTYPE" == 'darwin'* ]]; then
     if open -Ra "hammerspoon" ; then
-        rm -rf "$HOME/.hammerspoon"
-        ln -s "$dotfiles_dir/hammerspoon" "$HOME/.hammerspoon"
+        ln -fTs "$dotfiles_dir/hammerspoon" "$HOME/.hammerspoon"
         echo Created .hammerspoon folder symlink
     fi
 else
     if [ -d "$dotfiles_dir/arch/bin" ]; then
-        rm -rf "$HOME/bin"
-        ln -s "$dotfiles_dir/arch/bin" "$HOME/bin"
+        ln -fTs "$dotfiles_dir/arch/bin" "$HOME/bin"
         echo Created bin folder symlink
     fi
+    # Window manager related
     if type "i3" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/i3"
-        ln -s "$dotfiles_dir/arch/config/i3" "$HOME/.config/i3"
-        echo Created i3 folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/i3" "$HOME/.config/i3"
+        echo Created .config/i3 folder symlink
         if type "xfce4-power-manager" > /dev/null 2>&1; then
-            sudo rm -rf "/usr/local/bin/xflock4"
-            sudo ln -s "$dotfiles_dir/arch/config/i3/i3lock_fancy.sh" "/usr/local/bin/xflock4"
-            echo Created xflock4 symlink
+            sudo ln -fTs "$dotfiles_dir/arch/config/i3/i3lock_fancy.sh" "/usr/local/bin/xflock4"
+            echo Created /usr/local/bin/xflock4 symlink to i3lock
         fi
-        sudo rm -rf "/etc/systemd/system/sleeplock.service"
-        sudo ln -s "$dotfiles_dir/arch/systemd/sleeplock.service" "/etc/systemd/system/sleeplock.service"
-        echo Created sleeplock service symlink
+        sudo ln -fTs "$dotfiles_dir/arch/systemd/sleeplock.service" "/etc/systemd/system/sleeplock.service"
+        echo Created /etc/systemd/system/sleeplock.service symlink
     fi
     if type "polybar" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/polybar"
-        ln -s "$dotfiles_dir/arch/config/polybar" "$HOME/.config/polybar"
-        echo Created polybar folder symlink
-    fi
-    if type "pulseaudio" > /dev/null 2>&1; then
-        mkdir -p "$HOME/.pulse/"
-        rm -rf "$HOME/.pulse/default.pa"
-        ln -s "$dotfiles_dir/arch/pulse/default.pa" "$HOME/.pulse/default.pa"
-        echo Created pulseaudio default.pa folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/polybar" "$HOME/.config/polybar"
+        echo Created .config/polybar folder symlink
     fi
     if type "Xorg" > /dev/null 2>&1; then
-        rm -rf "$HOME/.xinitrc"
-        ln -s "$dotfiles_dir/arch/X/xinitrc" "$HOME/.xinitrc"
+        ln -fTs "$dotfiles_dir/arch/X/xinitrc" "$HOME/.xinitrc"
         echo Created .xinitrc symlink
-        rm -rf "$HOME/.Xresources"
-        ln -s "$dotfiles_dir/arch/X/xresources" "$HOME/.Xresources"
+        ln -fTs "$dotfiles_dir/arch/X/xresources" "$HOME/.Xresources"
         echo Created .Xresources symlink
-        rm -rf "$HOME/.Xmodmap"
-        ln -s "$dotfiles_dir/arch/X/xmodmap" "$HOME/.Xmodmap"
+        ln -fTs "$dotfiles_dir/arch/X/xmodmap" "$HOME/.Xmodmap"
         echo Created .Xmodmap symlink
     fi
+    if type "compton" > /dev/null 2>&1; then
+        ln -fTs "$dotfiles_dir/arch/config/compton.conf" "$HOME/.config/compton.conf"
+        echo Created .config/compton.conf symlink
+    fi
     if [ -d "$dotfiles_dir/arch/config/gtk" ]; then
-        rm -rf "$HOME/.config/gtk-3.0"
-        ln -s "$dotfiles_dir/arch/config/gtk/gtk-3.0" "$HOME/.config/gtk-3.0"
-        echo Created gtk-3.0 folder symlink
-        rm -rf "$HOME/.gtkrc-2.0"
-        ln -s "$dotfiles_dir/arch/config/gtk/gtkrc-2.0" "$HOME/.gtkrc-2.0"
+        ln -fTs "$dotfiles_dir/arch/config/gtk/gtk-3.0" "$HOME/.config/gtk-3.0"
+        echo Created .config/gtk-3.0 folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/gtk/gtkrc-2.0" "$HOME/.gtkrc-2.0"
         echo Created .gtkrc-2.0 symlink
     fi
     if [ -d "$dotfiles_dir/arch/fontconfig" ]; then
-        rm -rf "$HOME/fontconfig"
-        ln -s "$dotfiles_dir/arch/fontconfig" "$HOME/fontconfig"
+        ln -fTs "$dotfiles_dir/arch/fontconfig" "$HOME/fontconfig"
         echo Created fontconfig folder symlink
     fi
+    # Applications
+    if type "pulseaudio" > /dev/null 2>&1; then
+        mkdir -p "$HOME/.pulse/"
+        ln -fTs "$dotfiles_dir/arch/pulse/default.pa" "$HOME/.pulse/default.pa"
+        echo Created .pulse/default.pa symlink
+    fi
     if type "rofi" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/rofi"
-        ln -s "$dotfiles_dir/arch/config/rofi" "$HOME/.config/rofi"
-        echo Created rofi folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/rofi" "$HOME/.config/rofi"
+        echo Created .config/rofi folder symlink
     fi
     if type "dunst" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/dunst"
-        ln -s "$dotfiles_dir/arch/config/dunst" "$HOME/.config/dunst"
-        echo Created dunst folder symlink
-    fi
-    if type "compton" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/compton.conf"
-        ln -s "$dotfiles_dir/arch/config/compton.conf" "$HOME/.config/compton.conf"
-        echo Created compton config symlink
+        ln -fTs "$dotfiles_dir/arch/config/dunst" "$HOME/.config/dunst"
+        echo Created .config/dunst folder symlink
     fi
     if type "feh" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/feh"
-        ln -s "$dotfiles_dir/arch/config/feh" "$HOME/.config/feh"
-        echo Created feh folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/feh" "$HOME/.config/feh"
+        echo Created .config/feh folder symlink
     fi
     if type "zathura" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/zathura"
-        ln -s "$dotfiles_dir/arch/config/zathura" "$HOME/.config/zathura"
-        echo Created zathura folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/zathura" "$HOME/.config/zathura"
+        echo Created .config/zathura folder symlink
     fi
     if type "mpv" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/mpv"
-        ln -s "$dotfiles_dir/arch/config/mpv" "$HOME/.config/mpv"
-        echo Created mpv folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/mpv" "$HOME/.config/mpv"
+        echo Created .config/mpv folder symlink
     fi
     if type "onedrive" > /dev/null 2>&1; then
-        rm -rf "$HOME/.config/onedrive"
-        ln -s "$dotfiles_dir/arch/config/onedrive" "$HOME/.config/onedrive"
-        echo Created onedrive folder symlink
+        ln -fTs "$dotfiles_dir/arch/config/onedrive" "$HOME/.config/onedrive"
+        echo Created .config/onedrive folder symlink
     fi
 fi
 
+# Git
 if type "git" > /dev/null 2>&1; then
-    rm -rf "$HOME/.gitignore"
-    ln -s "$dotfiles_dir/gitignore" "$HOME/.gitignore"
+    ln -fTs "$dotfiles_dir/gitignore" "$HOME/.gitignore"
     echo Created .gitignore symlink
 
     read -p "Do you want to create new gitconfig file (y/n)? " -n 1 -r
