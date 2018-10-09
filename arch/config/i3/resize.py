b352de62b8e65f771fff38e069ef8c14d7b2134f
petobens <ferrari_pedro@yahoo.com>
2018-10-09 00:14:29 -0300

    i3: correct rectangle x position

--- arch/config/i3/resize.py
+++ arch/config/i3/resize.py
@@ -14,7 +14,7 @@ LAYOUT_DICT = {
     'Bottom Left': (0, 0.5, 0.5, 0.5),
     'Bottom Right': (0.5, 0.5, 0.5, 0.5),
     'Center': (0.25, 0.25, 0.5, 0.5),
-    'Rectangle': (0.15, 0.2, 0.75, 0.6),
+    'Rectangle': (0.125, 0.2, 0.75, 0.6),
     'Dialog': (0.33, 0.3, 0.35, 0.25),
     # This is hack to resize windows in HiDPI screens that were downscaled
     # to 1920x1080 and (for some strange reason) incorrectly rendered when
