04801cf24023f4dbdd75b634f212de5bce3de0d7
petobens <ferrari_pedro@yahoo.com>
2018-12-08 20:14:18 -0300

    python: improve ipython highlighting a bit to match vim

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -1,16 +1,21 @@
 import sys
-
 from operator import attrgetter
 
-from pygments.token import (
-    Comment, Error, Keyword, Literal, Name, Number, Operator, String, Text,
-    Token
-)
-
 import IPython.terminal.prompts as prompts
-
 from prompt_toolkit.application import get_app
 from prompt_toolkit.key_binding.vi_state import InputMode, ViState
+from pygments.token import (
+    Comment,
+    Error,
+    Keyword,
+    Literal,
+    Name,
+    Number,
+    Operator,
+    String,
+    Text,
+    Token,
+)
 
 c = get_config()  # noqa
 
@@ -43,8 +48,7 @@ class MyPrompt(prompts.Prompts):
     """Custom prompt with vi mode indicator"""
 
     def in_prompt_tokens(self):
-        mode = 'I' if get_app(
-        ).vi_state.input_mode == InputMode.INSERT else 'N'
+        mode = 'I' if get_app().vi_state.input_mode == InputMode.INSERT else 'N'
         return [
             (prompts.Token.Prompt, f'({mode})['),
             (prompts.Token.PromptNum, str(self.shell.execution_count)),
@@ -80,35 +84,40 @@ pmenu = '#333841'
 syntax_fg = white
 syntax_fold_bg = comment_grey
 
+# See: https://github.com/prompt-toolkit/python-prompt-toolkit/blob/master/prompt_toolkit/styles/defaults.py
 c.TerminalInteractiveShell.highlighting_style_overrides = {
     Text: syntax_fg,
     Error: red,
     Comment: comment_grey,
-    Keyword: purple,
-    Keyword.Constant: green,
+    Keyword: f'{purple} nobold',
+    Keyword.Constant: dark_yellow,
     Keyword.Namespace: purple,
-    Name.Namespace: syntax_fg,
+    Name.Namespace: f'{syntax_fg} nobold',
     Name.Builtin: red,
     Name.Function: light_blue,
-    Name.Class: light_blue,
+    Name.Class: f'{light_blue} nobold',
     Name.Decorator: light_blue,
     Name.Exception: yellow,
     Name.Variable.Magic: red,  # dunder methods
     Number: dark_yellow,
     Operator: purple,
-    Operator.Word: green,
+    Operator.Word: f'{purple} nobold',
     Literal: green,
+    Literal.String.Doc: f'{green} noitalic',
     String: green,
     Token.Prompt: green,
     Token.PromptNum: f'{green} bold',
     Token.OutPrompt: blue,
     Token.OutPromptNum: f'{blue} bold',
-    Token.Menu.Completions.Completion: f'bg:{pmenu} {syntax_fg}',
-    Token.Menu.Completions.Completion.Current: f'bg:{light_blue} {black}',
+    # FIXME: CompletionMenu don't seem to work
+    Token.CompletionMenu: f'bg:{pmenu} {syntax_fg}',
+    Token.CompletionMenu.Completion: f'bg:{pmenu} {syntax_fg}',
+    Token.CompletionMenu.Completion.Current: f'bg:{light_blue} {black}',
+    Token.CompletionMenu.Meta.Completion: f'bg:{pmenu} {syntax_fg}',
+    Token.CompletionMenu.Meta.Completion.Current: f'bg:{light_blue} {black}',
+    Token.CompletionMenu.MultiColumnMeta: f'bg:{light_blue} {black}',
     Token.MatchingBracket.Other: blue,
 }
 
 # Always import some modules
-c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-]
+c.InteractiveShellApp.exec_lines = ['import numpy as np']
