a463033991bff4c2319a7f7db8836d6fc068938a
petobens <ferrari_pedro@yahoo.com>
2021-11-05 19:54:40 -0300

    Moar

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -18,29 +18,47 @@ utils.keymap('n', '<C-p>', ':bp<CR>')
 -- Window movement
 utils.keymap('n', '<Leader>vs', ':vsplit<CR>')
 utils.keymap('n', '<Leader>sp', ':split<CR>')
-utils.keymap('n', '<c-h>', '<C-W>h')
-utils.keymap('n', '<c-j>', '<C-W>j')
-utils.keymap('n', '<c-k>', '<C-W>k')
-utils.keymap('n', '<c-l>', '<C-W>l')
-utils.keymap('n', '<c-c>', '<C-W>c')
+utils.keymap('n', '<C-h>', '<C-W>h')
+utils.keymap('n', '<C-j>', '<C-W>j')
+utils.keymap('n', '<C-k>', '<C-W>k')
+utils.keymap('n', '<C-l>', '<C-W>l')
+utils.keymap('n', '<C-c>', '<C-W>c')
 
 -- Line/Editing Movement
-utils.keymap('n', 'H', '0',  {noremap = false})
-utils.keymap('n', 'L', 'g_',  {noremap = false})
+utils.keymap('n', '<up>', '<nop>',  {noremap = false})
+utils.keymap('n', '<down>', '<nop>',  {noremap = false})
+utils.keymap('n', '<left>', '<nop>',  {noremap = false})
+utils.keymap('n', '<right>', '<nop>',  {noremap = false})
+utils.keymap('n', 'j', 'gj',  {noremap = false})
+utils.keymap('n', 'k', 'gk',  {noremap = false})
+utils.keymap('n', 'H', '^',  {noremap = false})
+utils.keymap('n', 'L', '$',  {noremap = false})
 utils.keymap('v', 'L', 'g_',  {noremap = false})
 utils.keymap('n', '<tab>', '%',  {noremap = false})
 utils.keymap('n', '<A-j>', ':move +1<CR>')
 utils.keymap('n', '<A-k>', ':move -2<CR>')
+-- FIXME: not working
+utils.keymap('v', '<A-j>', ':move +1<CR>')
+utils.keymap('v', '<A-k>', ':move -2<CR>')
 utils.keymap('n', '<A-s>', 'i<CR><ESC>^mwgk:silent! s/\v +$//<CR>:noh<CR>`w')
+utils.keymap('n', '<Leader>ic', ':set list!<CR>')
 
 -- Yanking pasting
 utils.keymap('n', 'Y', 'y$',  {noremap = false})
-utils.keymap('n', '<Leader>p', ':put<CR>',  {noremap = false})
-utils.keymap('n', '<Leader>P', ':put!<CR>',  {noremap = false})
+utils.keymap('n', '<Leader>p', ':put<CR>')
+utils.keymap('n', '<Leader>P', ':put!<CR>')
 
 -- Search
 utils.keymap('n', '<Leader><Space>', ':set hlsearch!<CR>')
+utils.keymap('n', '/', '/\\v', {silent = false, noremap = false})
+utils.keymap('n', '?', '?\\v', {silent = false, noremap = false})
+utils.keymap('v', '/', '/\\v', {silent = false, noremap = false})
+utils.keymap('v', '?', '?\\v', {silent = false, noremap = false})
+utils.keymap('n', 'n', 'nzzzv', {noremap = false})
+utils.keymap('n', 'N', 'Nzzzv', {noremap = false})
 
+-- Spelling
+utils.keymap('n', '<Leader>sc', ':set spell!<CR>')
 
 -- Commented
 -- FIXME
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,24 +1,73 @@
 local opt = require('utils').opt
 
+-- Vim behaviour
+opt('o', 'clipboard', 'unnamedplus')
+opt('o', 'modeline', false)
+opt('o', 'title', true)
+opt('o', 'cmdwinheight', 4)
+opt('o', 'lazyredraw', true)
+opt('o', 'visualbell', true)
+opt('o', 'shortmess', 'aoOtTIc')
+opt('o', 'confirm', true)
+opt('o', 'updatetime', 500)
+opt('o', 'timeoutlen', 550)
+opt('o', 'ttimeoutlen', 0)
+opt('o', 'foldlevelstart', 0)
+opt('o', 'foldopen', vim.o.foldopen..',insert,jump')
+opt('o', 'signcolumn', 'number')
+opt('o', 'diffopt', 'internal,filler,indent-heuristic,algorithm:histogram')
+
+-- Appearance
 opt('o', 'number', true)
 opt('o', 'relativenumber', true)
+-- TODO: show cursorline only on the current window
+opt('o', 'cursorline', true)
+opt('o', 'virtualedit', 'block,onemore')
+opt('o', 'startofline', true)
+opt('o', 'scrolloff', 3)
 opt('o', 'splitright', true)
-opt('o', 'clipboard', 'unnamedplus')
+opt('o', 'splitbelow', true)
+opt('o', 'winblend', 7)
+
+-- Read write
+opt('o', 'autowrite', true)
 
 -- Syntax
 opt('o', 'termguicolors', true)
+opt('o', 'iskeyword', vim.o.iskeyword..',:')
 
--- Tab settings
+-- TODO backups and undo
+
+
+-- Search, matching and substitution
+opt('o', 'ignorecase', true)
+opt('o', 'smartcase', true)
+-- opt('o', 'hlsearch', true)
+opt('o', 'gdefault', true)
+opt('o', 'showmatch', true)
+opt('o', 'matchtime', 1)
+
+-- Tab and indent
+opt('o', 'smartcase', true)
 opt('o', 'tabstop', 4)
 opt('o', 'shiftwidth', 4)
 opt('o', 'softtabstop', 4)
 opt('o', 'expandtab', true)
 opt('o', 'shiftround', true)
-
-
-opt('o', 'scrolloff', 3)
-
 opt('o', 'wrap', true)
 opt('o', 'linebreak', true)
 opt('o', 'breakindent', true)
 opt('o', 'autoindent', true)
+opt('o', 'textwidth', 80)
+opt('o', 'colorcolumn', '+1')
+opt('o', 'whichwrap', vim.o.whichwrap..',<,>,h,l,[,]')
+opt('o', 'listchars', 'tab:▸\\ ,eol:¬,trail:•,extends:»,precedes:«,nbsp:␣')
+
+-- Wildmenu
+opt('o', 'wildignorecase', true)
+opt('o', 'wildmode', 'longest:full,full')
+opt('o', 'wildignore', '*~,*.o,*.obj,*.dll,*.dat,*.swp,*.zip,*.exe,*.DS_Store,*.out,*.toc')
+
+-- Misc
+opt('o', 'spelllang', 'en,es')
+-- TODO: spellfile
