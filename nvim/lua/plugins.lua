1187ddc234cb48ceedcb2c1ad0bc964070660bd7
petobens <ferrari_pedro@yahoo.com>
2021-12-04 12:56:57 -0300

    Add again

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,13 +23,14 @@ return require('packer').startup(function(use)
             require('plugins/lualine')
         end,
     })
-    -- use({
-        -- 'romgrk/barbar.nvim',
-        -- requires = {'kyazdani42/nvim-web-devicons'},
-        -- config = function()
-            -- require('plugins/barbar')
-        -- end,
-    -- })
+    -- Maybbe try https://github.com/akinsho/bufferline.nvim
+    use({
+        'romgrk/barbar.nvim',
+        requires = {'kyazdani42/nvim-web-devicons'},
+        config = function()
+            require('plugins/barbar')
+        end,
+    })
 
     -- Editing
     use({
@@ -59,12 +60,12 @@ return require('packer').startup(function(use)
             require('plugins/high_str')
         end,
     })
-    -- use({
-        -- 'lukas-reineke/indent-blankline.nvim',
-        -- config = function()
-            -- require('plugins/indent_lines')
-        -- end,
-    -- })
+    use({
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require('plugins/indent_lines')
+        end,
+    })
 
     -- LSP and completion
     use({
