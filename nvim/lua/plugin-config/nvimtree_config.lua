93ae39b23c120a1b100eac4a648dd5b58584ecb4
petobens <ferrari_pedro@yahoo.com>
2021-12-12 22:06:10 -0300

    Nvim tree  nits

--- nvim/lua/plugin-config/nvimtree_config.lua
+++ nvim/lua/plugin-config/nvimtree_config.lua
@@ -3,9 +3,22 @@ local u = require('utils')
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_disable_window_picker = 1
 
+_G.NvimTreeConfig = {}
+function NvimTreeConfig.cd_or_open()
+    local lib = require('nvim-tree.lib')
+    local node = lib.get_node_at_cursor()
+    if node then
+        if node.entries then
+            require('nvim-tree').on_keypress('cd')
+        else
+            require('nvim-tree').on_keypress('edit')
+        end
+    end
+end
+
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 local map_list = {
-    {key = '<CR>', cb = tree_cb('cd')},
+    {key = '<CR>',  cb = ':lua NvimTreeConfig.cd_or_open()<CR>'},
     {key = 'v', cb = tree_cb('vsplit')},
     {key = 's', cb = tree_cb('split')},
     {key = 'F', cb = tree_cb('create')},
@@ -22,6 +35,8 @@ local map_list = {
 require('nvim-tree').setup({
     view = {
         width = 40,
+        numbers = true,
+        relativenumber = true,
         mappings = {
             custom_only = false,
             list = map_list
