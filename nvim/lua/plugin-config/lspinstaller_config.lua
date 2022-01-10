51a5d825035d7f4a441163e062b1e9624039c772
petobens <ferrari_pedro@yahoo.com>
2022-01-10 17:54:09 -0300

    Auto install lsp servers

--- nvim/lua/plugin-config/lspinstaller_config.lua
+++ nvim/lua/plugin-config/lspinstaller_config.lua
@@ -1,5 +1,20 @@
 local lsp_installer = require('nvim-lsp-installer')
 
+-- Auto install servers
+local servers = {
+    'sumneko_lua',
+}
+for _, name in pairs(servers) do
+    local server_is_found, server = lsp_installer.get_server(name)
+    if server_is_found then
+        if not server:is_installed() then
+            print('Installing ' .. name)
+            server:install()
+        end
+    end
+end
+
+-- Customize servers
 lsp_installer.on_server_ready(function(server)
     local opts = {}
 
