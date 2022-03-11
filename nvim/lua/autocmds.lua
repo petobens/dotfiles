6dbe6a8a17a1dbd313e220c13a2bdbf91c859098
petobens <ferrari_pedro@yahoo.com>
2022-03-11 00:08:11 -0300

    nvim: use lua autocmd api

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,60 +1,63 @@
 -- Save and load viewoptions and previous session
-vim.cmd([[
-augroup session
-    au!
-    au VimLeavePre * execute 'mksession! ' . v:lua.udfs.session_name()
-    au BufWinLeave {*.*,vimrc,vimrc_min,bashrc,config}
-        \ if &previewwindow != 1 | mkview | endif
-    "au BufWinEnter {*.*,vimrc,vimrc_min,bashrc,config}
-    "    \ if &previewwindow != 1 | silent! loadview | endif
-augroup END
-]])
+vim.api.nvim_create_augroup('session', { clear = true })
+vim.api.nvim_create_autocmd(
+    'VimLeavePre',
+    { group = 'session', command = [[execute 'mksession! ' . v:lua.udfs.session_name()]] }
+)
+vim.api.nvim_create_autocmd('BufWinLeave', {
+    group = 'session',
+    pattern = { '*.*', 'bashrc', 'config' },
+    command = 'if &previewwindow != 1 | mkview | endif',
+})
+-- vim.api.nvim_create_autocmd('BufWinEnter', {
+-- group = 'session',
+-- pattern = { '*.*', 'bashrc', 'config' },
+-- command = 'if &previewwindow != 1 | silent! loadview | endif',
+-- })
 
 -- Save when losing focus
-vim.cmd([[
-augroup focus_lost
-    au!
-    au FocusLost * silent! wall
-augroup END
-]])
+vim.api.nvim_create_augroup('focus_lost', { clear = true })
+vim.api.nvim_create_autocmd(
+    'FocusLost',
+    { group = 'focus_lost', command = 'silent! wall' }
+)
 
 -- Disable readonly warning
-vim.cmd([[
-augroup no_ro_warn
-    au!
-    au FileChangedRO * set noreadonly
-augroup END
-]])
+vim.api.nvim_create_augroup('no_ro_warn', { clear = true })
+vim.api.nvim_create_autocmd(
+    'FileChangedRO',
+    { group = 'no_ro_warn', command = 'set noreadonly' }
+)
 
 -- Resize splits when the Vim window is resized
-vim.cmd([[
-augroup vim_resized
-    au!
-    au VimResized * :wincmd =
-augroup END
-]])
+vim.api.nvim_create_augroup('vim_resized', { clear = true })
+vim.api.nvim_create_autocmd('VimResized', { group = 'vim_resized', command = 'wincmd =' })
 
 -- Only show cursorline in the current window
-vim.cmd([[
-augroup cline
-    au!
-    au WinLeave * setlocal nocursorline
-    au VimEnter,WinEnter,BufWinEnter * setlocal cursorline
-augroup END
-]])
+vim.api.nvim_create_augroup('cline', { clear = true })
+vim.api.nvim_create_autocmd(
+    'WinLeave',
+    { group = 'cline', command = 'setlocal nocursorline' }
+)
+vim.api.nvim_create_autocmd(
+    { 'VimEnter', 'WinEnter', 'BufWinEnter' },
+    { group = 'cline', command = 'setlocal cursorline' }
+)
 
 -- Create non-existing parent directory on save
-vim.cmd([[
-augroup create_dir_before_write
-    au!
-    au BufWritePre * call v:lua.udfs.mk_non_dir()
-augroup END
-]])
+vim.api.nvim_create_augroup('create_dir_before_write', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = 'create_dir_before_write',
+    callback = function()
+        udfs.mk_non_dir()
+    end,
+})
 
 -- Delete trailing whitespace
-vim.cmd([[
-augroup create_dir_before_write
-    au!
-    au BufWritePre * call v:lua.udfs.delete_trailing_whitespace()
-augroup END
-]])
+vim.api.nvim_create_augroup('delete_trailing', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = 'delete_trailing',
+    callback = function()
+        udfs.delete_trailing_whitespace()
+    end,
+})
