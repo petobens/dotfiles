57949dced648de010e27edf69804c754442b3c32
petobens <ferrari_pedro@yahoo.com>
2022-04-30 16:18:07 -0300

    vim: update lsp installer config

--- nvim/lua/plugin-config/lspinstaller_config.lua
+++ nvim/lua/plugin-config/lspinstaller_config.lua
@@ -1,30 +1,21 @@
 local lsp_installer = require('nvim-lsp-installer')
+local lspconfig = require('lspconfig')
 
--- Auto install servers
-local servers = {
-    'sumneko_lua',
-}
-for _, name in pairs(servers) do
-    local server_is_found, server = lsp_installer.get_server(name)
-    if server_is_found then
-        if not server:is_installed() then
-            print('Installing ' .. name)
-            server:install()
-        end
+-- Autoinstall servers
+lsp_installer.setup({
+    ensure_installed = { 'sumneko_lua' },
+})
+
+-- Client specific
+local function on_attach(client, bufnr)
+    if client.name == 'sumneko_lua' then
+        -- We use null-ls
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
     end
 end
 
--- Customize servers
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-
-    if server.name == 'sumneko_lua' then
-        opts = vim.tbl_deep_extend('force', require('lua-dev').setup(), opts)
-        opts.on_attach = function(client)
-            client.resolved_capabilities.document_formatting = false
-            client.resolved_capabilities.document_range_formatting = false
-        end
-    end
-
-    server:setup(opts)
-end)
+lspconfig.sumneko_lua.setup({
+    settings = require('lua-dev').setup().settings,
+    on_attach = on_attach,
+})
