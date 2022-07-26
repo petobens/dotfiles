e42d8f07e610277e5605d7e1a956d688e5ec8d0c
petobens <ferrari_pedro@yahoo.com>
2022-07-26 09:45:28 -0300

    vim: add packadd packer when bootstraping plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
         'https://github.com/wbthomason/packer.nvim',
         install_path,
     })
+    vim.cmd([[packadd packer.nvim]])
 end
 
 return require('packer').startup(function(use)
