9be10fb19db6859163b89470b452267968bc2745
petobens <ferrari_pedro@yahoo.com>
2022-08-13 19:30:47 -0300

    vim: update comment maps following api changes

--- nvim/lua/plugin-config/comment_config.lua
+++ nvim/lua/plugin-config/comment_config.lua
@@ -7,14 +7,14 @@ require('Comment').setup({
 u.keymap(
     'n',
     '<Leader>cc',
-    "v:count == 0 ? '<Plug>(comment_toggle_current_linewise)' : '<Plug>(comment_toggle_linewise_count)'",
+    "v:count == 0 ? '<Plug>(comment_toggle_linewise_current)' : '<Plug>(comment_toggle_linewise_count)'",
     { expr = true, remap = true, replace_keycodes = false }
 )
 u.keymap('x', 'cc', '<Plug>(comment_toggle_linewise_visual)')
 u.keymap(
     'n',
     '<Leader>cu',
-    "v:count == 0 ? '<Plug>(comment_toggle_current_linewise)' : '<Plug>(comment_toggle_linewise_count)'",
+    "v:count == 0 ? '<Plug>(comment_toggle_linewise_current)' : '<Plug>(comment_toggle_linewise_count)'",
     { expr = true, remap = true, replace_keycodes = false }
 )
 u.keymap('x', 'cu', '<Plug>(comment_toggle_linewise_visual)')
