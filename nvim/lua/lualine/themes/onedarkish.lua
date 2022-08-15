31ddc6ab987412db4b539391d554e6552282cfc0
petobens <ferrari_pedro@yahoo.com>
2022-08-15 11:34:53 -0300

    vim: revert onedarkish lualine onedark palette call

--- nvim/lua/lualine/themes/onedarkish.lua
+++ nvim/lua/lualine/themes/onedarkish.lua
@@ -1,4 +1,37 @@
-local c = require('onedarkpro').get_colors()
+--- We cannot call `require('onedarkpro').get_colors()` here as per
+--- https://github.com/olimorris/onedarkpro.nvim/pull/65#issuecomment-1214244012
+--- So we copy the palette definition again
+local palette = {
+    bg = '#24272e',
+    fg = '#abb2bf',
+
+    -- Base colors
+    black = '#24272e',
+    blue = '#528bff',
+    cyan = '#56b6c2',
+    gray = '#5c6370',
+    green = '#98c379',
+    orange = '#d19a66',
+    purple = '#c678dd',
+    red = '#e06c75',
+    white = '#abb2bf',
+    yellow = '#e5c07b',
+    none = 'NONE',
+
+    -- Custom Colors
+    cursor_grey = '#282c34',
+    dark_red = '#be5046',
+    light_blue = '#61afef',
+    linenr_grey = '#4b5263',
+    pmenu = '#333841',
+    special_grey = '#3b4048',
+    visual_grey = '#3e4452',
+}
+palette.comment_grey = palette.gray
+palette.color_column = palette.cursor_grey
+palette.cursorline = palette.cursor_grey
+palette.highlight = palette.orange
+local c = palette
 
 local onedarkish = {}
 
