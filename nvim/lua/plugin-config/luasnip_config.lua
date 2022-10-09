c9acede119f3919cc84f7c6274b47b02ae4adfc4
petobens <ferrari_pedro@yahoo.com>
2022-10-09 09:58:45 -0300

    vim: working snippets config and boilerplate files

--- nvim/lua/plugin-config/luasnip_config.lua
+++ nvim/lua/plugin-config/luasnip_config.lua
@@ -1,9 +1,34 @@
+local luasnip = require('luasnip')
 local u = require('utils')
 
 require('luasnip.loaders.from_lua').lazy_load({
-    paths = vim.fn.stdpath('config') .. '/snippets/',
+    paths = { vim.fn.stdpath('config') .. '/snippets/' },
 })
 
+-- Mappings
+u.keymap({ 'i', 's' }, '<C-s>', function()
+    if luasnip.expandable() then
+        luasnip.expand({})
+    end
+end)
+u.keymap({ 'i', 's' }, '<C-j>', function()
+    if luasnip.jumpable(1) then
+        luasnip.jump(1)
+    end
+end)
+u.keymap({ 'i', 's' }, '<C-k>', function()
+    if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    end
+end)
 u.keymap('n', '<Leader>es', function()
-    require('luasnip.loaders.from_lua').edit_snippet_files()
+    require('luasnip.loaders').edit_snippet_files({
+        edit = function(file)
+            local split = 'split '
+            if vim.fn.winwidth(0) > 2 * (vim.go.textwidth or 80) then
+                split = 'vsplit '
+            end
+            vim.cmd(split .. file)
+        end,
+    })
 end)
