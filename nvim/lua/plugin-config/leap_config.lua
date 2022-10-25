deef4e93934612b69883d9c33dbc1250aa2b71ce
petobens <ferrari_pedro@yahoo.com>
2022-10-25 12:52:29 -0300

    vim: use leap.opts to override special keys

--- nvim/lua/plugin-config/leap_config.lua
+++ nvim/lua/plugin-config/leap_config.lua
@@ -2,15 +2,12 @@ local leap = require('leap')
 
 leap.setup({
     max_highlighted_traversal_targets = 20, -- show 20 matches when traversing
-    special_keys = {
-        repeat_search = '<enter>',
-        next_target = { '<A-n>' },
-        prev_target = { '<A-p>' },
-    },
 })
 require('flit').setup({}) -- enhanced f,F,t and T motions (as in sneak)
 
 -- Mappings (use default but don't map x)
 leap.add_default_mappings()
+leap.opts.special_keys.next_target = { '<A-n>' }
+leap.opts.special_keys.prev_target = { '<A-p>' }
 vim.keymap.del({ 'x', 'o' }, 'x')
 vim.keymap.del({ 'x', 'o' }, 'X')
