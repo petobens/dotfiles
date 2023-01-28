b8570d0097b78a8321d5180e282991a51243a676
petobens <ferrari_pedro@yahoo.com>
2023-01-28 12:54:52 -0300

    vim: use rounded corners for neozoom float

--- nvim/lua/plugin-config/neozoom_config.lua
+++ nvim/lua/plugin-config/neozoom_config.lua
@@ -3,8 +3,8 @@ local u = require('utils')
 require('neo-zoom').setup({
     left_ratio = 0.1,
     width_ratio = 0.8,
-    top_ratio = 0.1,
-    height_ratio = 0.9,
+    height_ratio = 0.91,
+    border = 'rounded',
 })
 
 u.keymap('n', '<Leader>zw', ':NeoZoomToggle<CR>')
