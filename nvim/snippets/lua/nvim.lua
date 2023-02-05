86571eb465e83f765a8e530bba545ca3d4bfd008
petobens <ferrari_pedro@yahoo.com>
2023-02-05 10:54:42 -0300

    vim: add lua pretty print snippet with vim.inspect

--- nvim/snippets/lua/nvim.lua
+++ nvim/snippets/lua/nvim.lua
@@ -1,10 +1,14 @@
 local ls = require('luasnip')
-local s = ls.snippet
+local f = ls.function_node
 local i = ls.insert_node
+local s = ls.snippet
 local fmta = require('luasnip.extras.fmt').fmta
 local rep = require('luasnip.extras').rep
 local line_begin = require('luasnip.extras.expand_conditions').line_begin
 
+local visual_selection = function(_, snip)
+    return snip.env.TM_SELECTED_TEXT[1] or {}
+end
 return {
     s(
         { trig = 'dd', dscr = 'Disable next line diagnostic' },
@@ -75,6 +79,20 @@ return {
                 i(3, 'event'),
                 rep(1),
                 i(4),
+            }
+        ),
+        { condition = line_begin }
+    ),
+    s(
+        { trig = 'pp', dscr = 'Pretty print' },
+        fmta(
+            [[
+                print(vim.inspect(<><>))<>
+            ]],
+            {
+                f(visual_selection),
+                i(1),
+                i(0),
             }
         ),
         { condition = line_begin }
