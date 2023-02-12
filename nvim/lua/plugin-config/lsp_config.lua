edadcaf2adf328e08a94a49fad6967118cb7e461
petobens <ferrari_pedro@yahoo.com>
2023-02-12 07:54:17 -0300

    vim: sumneko lua was renamed to lua-ls

--- nvim/lua/plugin-config/lsp_config.lua
+++ nvim/lua/plugin-config/lsp_config.lua
@@ -98,7 +98,7 @@ lspconfig.bashls.setup({
     on_attach = on_attach,
 })
 ---- Lua
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
     on_attach = on_attach,
     settings = {
         Lua = {
