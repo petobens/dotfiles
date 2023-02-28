8e974fb5e9583ffd8fa2359262a2016a41243a28
petobens <ferrari_pedro@yahoo.com>
2023-02-28 19:34:55 -0300

    vim: update nvimtree mappings to use on_attach function

--- nvim/lua/plugin-config/nvimtree_config.lua
+++ nvim/lua/plugin-config/nvimtree_config.lua
@@ -6,9 +6,7 @@ local u = require('utils')
 
 local Path = require('plenary.path')
 
-_G.NvimTreeConfig = {}
-
-function NvimTreeConfig.cd_find_file()
+local function cd_find_file()
     vim.cmd('NvimTreeFindFile')
     vim.cmd('sleep 3m') -- we seem to need this to allow focus
     local node = tree_api.get_node_under_cursor()
@@ -28,7 +26,7 @@ function NvimTreeConfig.cd_find_file()
     tree_api.find_file(node.name)
 end
 
-function NvimTreeConfig.cd_or_open()
+local function cd_or_open()
     local node = tree_api.get_node_under_cursor()
     if node then
         if node.nodes then
@@ -41,7 +39,7 @@ function NvimTreeConfig.cd_or_open()
     end
 end
 
-function NvimTreeConfig.up_dir()
+local function up_dir()
     local node = tree_api.get_node_under_cursor()
     local p = Path:new(node.absolute_path):parent()
     local dir = vim.fn.fnamemodify(tostring(p), ':t')
@@ -52,12 +50,12 @@ function NvimTreeConfig.up_dir()
     tree_api.find_file(dir)
 end
 
-function NvimTreeConfig.mark_down()
+local function mark_down()
     marks_api.toggle()
     vim.cmd('normal! j')
 end
 
-function NvimTreeConfig.telescope(picker, opts)
+local function telescope(picker, opts)
     opts = opts or {}
     opts.attach_mappings = function(_, map)
         map('i', '<CR>', _G.TelescopeConfig.custom_actions.open_nvimtree)
@@ -66,7 +64,7 @@ function NvimTreeConfig.telescope(picker, opts)
     _G.TelescopeConfig[picker](opts)
 end
 
-function NvimTreeConfig.telescope_preview()
+local function telescope_preview()
     local node = tree_api.get_node_under_cursor()
     local picker = 'find_files_cwd'
     if node.nodes then
@@ -86,12 +84,12 @@ function NvimTreeConfig.telescope_preview()
     })
 end
 
-function NvimTreeConfig.execute(cmd)
+local function execute(cmd)
     local node = tree_api.get_node_under_cursor()
     vim.fn.jobstart(cmd .. ' ' .. node.absolute_path)
 end
 
-function NvimTreeConfig.trash()
+local function trash()
     local nodes = marks_api.list()
     local conf_msg = string.format('Trash %s files? [y/n] ', #nodes)
     if next(nodes) == nil then
@@ -110,7 +108,7 @@ function NvimTreeConfig.trash()
     marks_api.clear()
 end
 
-function NvimTreeConfig.copy_move(action)
+local function copy_move(action)
     local fn = fs_api[action]
     if action == 'copy' then
         fn = fn.node
@@ -126,75 +124,83 @@ function NvimTreeConfig.copy_move(action)
     end
 end
 
-function NvimTreeConfig.paste()
+local function paste()
     fs_api.paste()
     marks_api.clear()
 end
 
-local tree_cb = require('nvim-tree.config').nvim_tree_callback
-local map_list = {
+local function on_attach(bufnr)
+    local map_opts = { buffer = bufnr }
     -- Tree
-    { key = 'q', cb = tree_cb('close') },
-    { key = '<Esc>', cb = tree_cb('close') },
-    { key = '<C-r>', cb = tree_cb('refresh') },
+    u.keymap('n', 'q', tree_api.close, map_opts)
+    u.keymap('n', '<ESC>', tree_api.close, map_opts)
+    u.keymap('n', '<C-r>', tree_api.reload, map_opts)
     -- Editing
-    { key = '<CR>', cb = ':lua NvimTreeConfig.cd_or_open()<CR>' },
-    { key = 'v', cb = tree_cb('vsplit') },
-    { key = 's', cb = tree_cb('split') },
-    { key = 'o', cb = tree_cb('system_open') },
+    u.keymap('n', '<CR>', cd_or_open, map_opts)
+    u.keymap('n', 'v', node_api.open.vertical, map_opts)
+    u.keymap('n', 's', node_api.open.horizontal, map_opts)
+    u.keymap('n', 'o', node_api.run.system, map_opts)
     -- Filesystem
-    { key = 'F', cb = tree_cb('create') },
-    { key = 'D', cb = tree_cb('create') },
-    { key = 'd', cb = ':lua NvimTreeConfig.trash()<CR>' },
-    { key = 'c', cb = ':lua NvimTreeConfig.copy_move("copy")<CR>' },
-    { key = 'm', cb = ':lua NvimTreeConfig.copy_move("cut")<CR>' },
-    { key = 'p', cb = ':lua NvimTreeConfig.paste()<CR>' },
-    { key = 'r', cb = tree_cb('rename') },
-    { key = 'y', cb = tree_cb('copy_name') },
-    { key = 'Y', cb = tree_cb('copy_absolute_path') },
-    { key = '<C-o>', cb = tree_cb('cd') },
-    { key = 'u', cb = ':lua NvimTreeConfig.up_dir()<CR>' },
-    {
-        key = 'h',
-        cb = ':lua require("nvim-tree.api").tree.change_root(vim.env.HOME)<CR>',
-    },
-    { key = '<A-i>', cb = tree_cb('toggle_file_info') },
-    { key = ',th', cb = tree_cb('toggle_dotfiles') },
-    { key = ',ti', cb = tree_cb('toggle_git_ignored') },
+    u.keymap('n', 'F', fs_api.create, map_opts)
+    u.keymap('n', 'D', fs_api.create, map_opts)
+    u.keymap('n', 'd', trash, map_opts)
+    u.keymap('n', 'c', function()
+        copy_move('copy')
+    end, map_opts)
+    u.keymap('n', 'm', function()
+        copy_move('cut')
+    end, map_opts)
+    u.keymap('n', 'p', paste, map_opts)
+    u.keymap('n', 'r', fs_api.rename, map_opts)
+    u.keymap('n', 'y', fs_api.copy.filename, map_opts)
+    u.keymap('n', 'Y', fs_api.copy.absolute_path, map_opts)
+    u.keymap('n', '<C-o>', tree_api.change_root_to_node, map_opts)
+    u.keymap('n', 'u', up_dir, map_opts)
+    u.keymap('n', 'h', function()
+        tree_api.change_root(vim.env.HOME)
+    end, map_opts)
+    u.keymap('n', '<A-i>', node_api.show_info_popup, map_opts)
+    u.keymap('n', ',th', tree_api.toggle_hidden_filter, map_opts)
+    u.keymap('n', ',ti', tree_api.toggle_gitignore_filter, map_opts)
     -- Folds/marks
-    { key = 'zc', cb = tree_cb('close_node') },
-    { key = 'zo', cb = tree_cb('edit') },
-    { key = 'zm', cb = ':lua require("nvim-tree.api").tree.collapse_all()<CR>' },
-    { key = 'zr', cb = ':lua require("nvim-tree.api").tree.expand_all()<CR>' },
-    { key = '<Space>', cb = ':lua NvimTreeConfig.mark_down()<CR>' },
+    u.keymap('n', 'zc', node_api.navigate.parent_close, map_opts)
+    u.keymap('n', 'zo', node_api.open.edit, map_opts)
+    u.keymap('n', 'zm', tree_api.collapse_all, map_opts)
+    u.keymap('n', 'zr', tree_api.expand_all, map_opts)
+    u.keymap('n', '<Space>', mark_down, map_opts)
     -- Telescope integration
-    { key = '<C-t>', cb = ':lua NvimTreeConfig.telescope("find_files_cwd")<CR>' },
-    {
-        key = '<A-t>',
-        cb = ':lua NvimTreeConfig.telescope("find_files_cwd", { no_ignore = true })<CR>',
-    },
-    { key = '<A-c>', cb = ':lua NvimTreeConfig.telescope("find_dirs")<CR>' },
-    { key = '<A-p>', cb = ':lua NvimTreeConfig.telescope("parent_dirs")<CR>' },
-    { key = '<A-z>', cb = ':lua NvimTreeConfig.telescope("z_with_tree_preview")<CR>' },
-    { key = 'b', cb = ':lua NvimTreeConfig.telescope("bookmark_dirs")<CR>' },
-    { key = '<A-v>', cb = ':lua NvimTreeConfig.telescope_preview()<CR>' },
+    u.keymap('n', '<C-t>', function()
+        telescope('find_files_cwd')
+    end, map_opts)
+    u.keymap('n', '<A-t>', function()
+        telescope('find_files_cwd', { no_ignore = true })
+    end, map_opts)
+    u.keymap('n', '<A-c>', function()
+        telescope('find_dirs')
+    end, map_opts)
+    u.keymap('n', '<A-p>', function()
+        telescope('parent_dirs')
+    end, map_opts)
+    u.keymap('n', '<A-z>', function()
+        telescope('z_with_tree_preview')
+    end, map_opts)
+    u.keymap('n', 'b', function()
+        telescope('bookmark_dirs')
+    end, map_opts)
+    u.keymap('n', '<A-v>', telescope_preview, map_opts)
     -- System
-    {
-        key = ',od',
-        cb = ':lua NvimTreeConfig.execute("dragon-drop -a -x")<CR>',
-    },
-}
+    u.keymap('n', ',od', function()
+        execute('dragon-drop -a -x')
+    end, map_opts)
+end
 
 require('nvim-tree').setup({
     disable_netrw = false, -- conflicts with Fugitive's Gbrowse
+    on_attach = on_attach,
     view = {
         width = { min = 43, max = 55 },
         number = true,
         relativenumber = true,
-        mappings = {
-            custom_only = true,
-            list = map_list,
-        },
     },
     renderer = {
         root_folder_label = function(path)
@@ -254,4 +260,4 @@ require('nvim-tree').setup({
     diagnostics = { enable = false },
 })
 
-u.keymap('n', '<Leader>ff', NvimTreeConfig.cd_find_file)
+u.keymap('n', '<Leader>ff', cd_find_file)
