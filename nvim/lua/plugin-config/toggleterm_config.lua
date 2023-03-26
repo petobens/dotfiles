9a30eef071d1984e42ff181770330d47f23ff0e8
petobens <ferrari_pedro@yahoo.com>
2023-03-26 15:50:43 -0300

    vim: switch to insert mode whenever entering a terminal buffer

--- nvim/lua/plugin-config/toggleterm_config.lua
+++ nvim/lua/plugin-config/toggleterm_config.lua
@@ -18,9 +18,21 @@ require('toggleterm').setup({
     },
 })
 
+-- Get into insert mode whenever we enter a terminal buffer
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    group = vim.api.nvim_create_augroup('TermAcg', { clear = true }),
+    pattern = { '*' },
+    callback = function()
+        if vim.bo.buftype == 'terminal' then
+            vim.cmd('startinsert')
+        end
+    end,
+})
+
+-- Mappings
 u.keymap('n', '<Leader>st', ':ToggleTerm direction=horizontal<CR>')
 u.keymap('n', '<Leader>vt', ':ToggleTerm direction=vertical<CR>')
 u.keymap('n', '<Leader>tc', ':TermExec cmd="exit"<CR>')
 u.keymap('n', '<Leader>tw', ':TermExec cmd="clear"<CR>')
-u.keymap('n', '<Leader>ri', ':ToggleTermSendCurrentLine<CR>')
+u.keymap('n', '<Leader>rl', ':ToggleTermSendCurrentLine<CR>')
 u.keymap('v', '<Leader>ri', ':ToggleTermSendVisualSelection<CR>')
