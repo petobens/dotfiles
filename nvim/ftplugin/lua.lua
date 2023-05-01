9ff09a7435d9e11fe95dcd31dcc84a89e5bfa664
petobens <ferrari_pedro@yahoo.com>
2023-05-01 19:45:21 -0300

    vim: add more functions/mappings to run lua code
    To (almost) match what we have for python

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,5 +1,6 @@
 local overseer = require('overseer')
 local u = require('utils')
+local utils = require('telescope.utils')
 
 -- Options
 vim.opt_local.formatoptions = 'jcql'
@@ -13,6 +14,7 @@ local _parse_qf = function(qf_title, active_window_id)
             table.insert(new_qf, v)
         end
     end
+    table.remove(new_qf) -- remove last element since its the "Process Exited" message
     if next(new_qf) ~= nil then
         vim.fn.setqflist({}, ' ', { items = new_qf, title = qf_title })
         vim.cmd('copen')
@@ -20,7 +22,7 @@ local _parse_qf = function(qf_title, active_window_id)
     end
 end
 
-local function run_lua()
+local function run_overseer()
     local current_win_id = vim.fn.win_getid()
     vim.cmd('silent noautocmd update')
     overseer.run_template({ name = 'run_lua' }, function(task)
@@ -31,11 +33,34 @@ local function run_lua()
     end)
 end
 
+local run_toggleterm = function()
+    vim.cmd('silent noautocmd update')
+    vim.cmd(
+        string.format(
+            'TermExec cmd="%s %s"',
+            'nvim -ll',
+            vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':p')
+        )
+    )
+end
+
+local run_tmux_pane = function()
+    if vim.env.TMUX == nil then
+        return
+    end
+    local cwd = utils.buffer_dir()
+    local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':t')
+    local sh_cmd = '"nvim -ll ' .. fname .. [[; read -p ''"]]
+    vim.cmd('silent! !tmux new-window -c ' .. cwd .. ' -n ' .. fname .. ' ' .. sh_cmd)
+end
+
 -- Mappings
-u.keymap({ 'n', 'i' }, '<F7>', run_lua, { buffer = true })
+u.keymap({ 'n', 'i' }, '<F7>', run_overseer, { buffer = true })
+u.keymap({ 'n', 'i' }, '<F5>', run_tmux_pane, { buffer = true })
+u.keymap('n', '<Leader>rf', run_toggleterm, { buffer = true })
 u.keymap(
     'n',
-    '<Leader>rf',
+    '<Leader>ri',
     '<Cmd>update<CR>:luafile %<CR>',
     { silent = false, buffer = true }
 )
