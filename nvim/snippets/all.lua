958b709f625fa7e6b23b7cc44e29f6cf097ca5a1
petobens <ferrari_pedro@yahoo.com>
2023-05-31 22:59:37 -0300

    vim: format all.lua snippets a bit

--- nvim/snippets/all.lua
+++ nvim/snippets/all.lua
@@ -5,12 +5,12 @@ local i = ls.insert_node
 local f = ls.function_node
 
 -- Functions
-local get_comment_string = function()
+local function get_comment_string()
     ---@diagnostic disable-next-line: missing-parameter
     return vim.trim(vim.split(vim.bo.cms, '%%s')[1])
 end
 
-local visual_selection = function(_, snip)
+local function visual_selection(_, snip)
     return snip.env.TM_SELECTED_TEXT or {}
 end
 
@@ -27,57 +27,56 @@ return {
         f(visual_selection),
         i(0),
     }),
-},
-    {
-        -- Autosnippets
-        ---- "Abolish" type spelling mistakes
-        s('anio', { t('año') }),
-        s('campaing', { t('campaign') }),
-        s('teh', { t('the') }),
-        s('widht', { t('width') }),
+}, {
+    -- Autosnippets
+    ---- "Abolish" type spelling mistakes
+    s('anio', { t('año') }),
+    s('campaing', { t('campaign') }),
+    s('teh', { t('the') }),
+    s('widht', { t('width') }),
 
-        ---- Autopairs
-        s({ trig = 'dq', dscr = 'Double quotes' }, {
-            t('"'),
-            f(visual_selection),
-            i(1),
-            t('"'),
-            i(0),
-        }),
-        s({ trig = 'sq', dscr = 'Single quotes' }, {
-            t("'"),
-            f(visual_selection),
-            i(1),
-            t("'"),
-            i(0),
-        }),
-        s({ trig = '{{', wordTrig = false, dscr = 'Braces' }, {
-            t('{'),
-            f(visual_selection),
-            i(1),
-            t('}'),
-            i(0),
-        }),
-        s({ trig = '((', wordTrig = false, dscr = 'Parenthesis' }, {
-            t('('),
-            f(visual_selection),
-            i(1),
-            t(')'),
-            i(0),
-        }),
-        s({ trig = '[[', wordTrig = false, dscr = 'Brackets' }, {
-            t('['),
-            f(visual_selection),
-            i(1),
-            t(']'),
-            i(0),
-        }),
+    ---- Autopairs
+    s({ trig = 'dq', dscr = 'Double quotes' }, {
+        t('"'),
+        f(visual_selection),
+        i(1),
+        t('"'),
+        i(0),
+    }),
+    s({ trig = 'sq', dscr = 'Single quotes' }, {
+        t("'"),
+        f(visual_selection),
+        i(1),
+        t("'"),
+        i(0),
+    }),
+    s({ trig = '{{', wordTrig = false, dscr = 'Braces' }, {
+        t('{'),
+        f(visual_selection),
+        i(1),
+        t('}'),
+        i(0),
+    }),
+    s({ trig = '((', wordTrig = false, dscr = 'Parenthesis' }, {
+        t('('),
+        f(visual_selection),
+        i(1),
+        t(')'),
+        i(0),
+    }),
+    s({ trig = '[[', wordTrig = false, dscr = 'Brackets' }, {
+        t('['),
+        f(visual_selection),
+        i(1),
+        t(']'),
+        i(0),
+    }),
 
-        s({ trig = '<<', wordTrig = false, dscr = '<>' }, {
-            t('<'),
-            f(visual_selection),
-            i(1),
-            t('>'),
-            i(0),
-        }),
-    }
+    s({ trig = '<<', wordTrig = false, dscr = '<>' }, {
+        t('<'),
+        f(visual_selection),
+        i(1),
+        t('>'),
+        i(0),
+    }),
+}
