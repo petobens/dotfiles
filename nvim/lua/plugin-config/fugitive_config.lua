839bad6f6bed8c079d26e49cc7233a286067bd9e
petobens <ferrari_pedro@yahoo.com>
2023-06-17 19:23:27 -0300

    vim: move to the first section of fugitive git status upon open

--- nvim/lua/plugin-config/fugitive_config.lua
+++ nvim/lua/plugin-config/fugitive_config.lua
@@ -87,7 +87,7 @@ vim.api.nvim_create_autocmd('FileType', {
 -- Mappings
 u.keymap('n', '<Leader>gd', '<Cmd>Gdiffsplit<CR><Cmd>wincmd x<CR>')
 u.keymap('n', '<Leader>gD', ':Git diff<space>', { silent = false })
-u.keymap('n', '<Leader>gs', '<Cmd>botright Git<CR><Cmd>wincmd J<bar>15 wincmd _<CR>')
+u.keymap('n', '<Leader>gs', '<Cmd>botright Git<CR><Cmd>wincmd J<bar>15 wincmd _<CR>4j')
 u.keymap('n', '<Leader>gc', '<Cmd>w!<CR><Cmd>Git commit<CR>')
 u.keymap('n', '<Leader>gM', '<Cmd>Git! mergetool<CR>')
 u.keymap('n', '<Leader>gr', ':Git rebase -i<space>', { silent = false })
