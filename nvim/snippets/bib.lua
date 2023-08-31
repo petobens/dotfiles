c434afc5bd2a4fe42c6e428a78d1a60b0a91efea
petobens <ferrari_pedro@yahoo.com>
2023-08-31 18:46:41 -0300

    vim: add bibkey bib snippet function

--- nvim/snippets/bib.lua
+++ nvim/snippets/bib.lua
@@ -1,33 +1,57 @@
+local extras = require('luasnip.extras')
 local ls = require('luasnip')
-local s = ls.snippet
+
+local f = ls.function_node
 local i = ls.insert_node
+local s = ls.snippet
+
+local l = extras.lambda
 local fmta = require('luasnip.extras.fmt').fmta
 local line_begin = require('luasnip.extras.expand_conditions').line_begin
+
+local function bibkey(node_idx)
+    local authors = node_idx[1][1]
+    if not authors or authors == '' then
+        return ''
+    end
+
+    local first_author = vim.fn.split(authors:lower(), 'and')[1]
+    local first_author_surname
+    if string.find(first_author, ',') then
+        first_author_surname = vim.fn.split(first_author, ',')[1]
+    else
+        first_author_surname = vim.fn.split(first_author, ' ')
+        first_author_surname = first_author_surname[#first_author_surname]
+    end
+    first_author_surname = first_author_surname:gsub('%s+', '')
+    return first_author_surname
+end
 
 return {
     s(
         { trig = 'art', dscr = 'Article' },
         fmta(
             [[
-            @Article{<>,
-                author  = {<>},
-                title   = {<>},
-                journal = {<>},
-                year    = {<>},
-                volume  = {<>},
-                number  = {<>},
-                pages   = {<>}
+            @Article{<><>,
+              author  = {<>},
+              title   = {<>},
+              journal = {<>},
+              year    = {<>},
+              volume  = {<>},
+              number  = {<>},
+              pages   = {<>}
             }
             ]],
             {
+                f(bibkey, { 1 }),
+                l(l._1:sub(-2), 4),
                 i(1),
-                i(2, 'names separated by "and"'),
-                i(3, 'title'),
-                i(4, 'journal name'),
-                i(5, 'year'),
-                i(6, 'volume number'),
-                i(7, 'number'),
-                i(8, 'page range'),
+                i(2),
+                i(3),
+                i(4),
+                i(5),
+                i(6),
+                i(7),
             }
         ),
         { condition = line_begin }
