ccd9854357c2ff145600964f85cb9014eb6d6f9b
petobens <ferrari_pedro@yahoo.com>
2023-09-29 22:43:20 -0300

    vim: minor conform config refactor

--- nvim/lua/plugin-config/conform_config.lua
+++ nvim/lua/plugin-config/conform_config.lua
@@ -1,28 +1,57 @@
 local u = require('utils')
 
+local conform = require('conform')
+local cu = require('conform.util')
+local formatters = require('conform.formatters')
+
 -- Global options
 vim.o.formatexpr = [[v:lua.require('conform').formatexpr()]]
 
+-- Formatters args
+---- Json
+cu.add_formatter_args(formatters.jq, { '--indent', '4' })
+---- Lua
+cu.add_formatter_args(
+    formatters.stylua,
+    { '--config-path=' .. vim.env.HOME .. '/.config/stylua.toml' }
+)
+---- Python
+cu.add_formatter_args(
+    formatters.isort,
+    { '--settings-file=' .. vim.env.HOME .. '/.isort.cfg' }
+)
+cu.add_formatter_args(
+    formatters.black,
+    { '--config=' .. vim.env.HOME .. '/.config/.black.toml' }
+)
+---- sh
+cu.add_formatter_args(formatters.shfmt, { '-i', '4', '-ci', '-sr' })
+---- TOML
+formatters.taplo.args = { 'format', '--config=' .. vim.env.HOME .. '/taplo.toml', '-' }
+
+-- Custom Formatters
+local custom_formatters = {
+    sqlfluff = {
+        command = 'sqlfluff',
+        args = {
+            'fix',
+            '--disable-progress-bar',
+            '-f',
+            '-n',
+            '-',
+        },
+        stdin = true,
+    },
+}
+
 -- Setup
-require('conform').setup({
+conform.setup({
     format_on_save = {
         timeout_ms = 200,
         lsp_fallback = false,
     },
     notify_on_error = false,
-    formatters = {
-        sqlfluff = {
-            command = 'sqlfluff',
-            args = {
-                'fix',
-                '--disable-progress-bar',
-                '-f',
-                '-n',
-                '-',
-            },
-            stdin = true,
-        },
-    },
+    formatters = custom_formatters,
     formatters_by_ft = {
         ['_'] = { 'trim_whitespace' },
         json = { 'jq' },
@@ -35,31 +64,7 @@ require('conform').setup({
     },
 })
 
--- Formatters args
-local cu = require('conform.util')
-local formatters = require('conform.formatters')
---- Json
-cu.add_formatter_args(formatters.jq, { '--indent', '4' })
---- Lua
-cu.add_formatter_args(
-    formatters.stylua,
-    { '--config-path=' .. vim.env.HOME .. '/.config/stylua.toml' }
-)
---- Python
-cu.add_formatter_args(
-    formatters.isort,
-    { '--settings-file=' .. vim.env.HOME .. '/.isort.cfg' }
-)
-cu.add_formatter_args(
-    formatters.black,
-    { '--config=' .. vim.env.HOME .. '/.config/.black.toml' }
-)
---- sh
-cu.add_formatter_args(formatters.shfmt, { '-i', '4', '-ci', '-sr' })
---- TOML
-formatters.taplo.args = { 'format', '--config=' .. vim.env.HOME .. '/taplo.toml', '-' }
-
--- Maps
+-- Mappings
 u.keymap({ 'n', 'v' }, '<Leader>fc', function()
-    require('conform').format({ async = true, lsp_fallback = false })
+    conform.format({ async = true, lsp_fallback = false })
 end)
