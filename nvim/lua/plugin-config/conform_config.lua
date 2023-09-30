a4d5703d6dd7fcdcbb44f95b94850d5d0fccd1d6
petobens <ferrari_pedro@yahoo.com>
2023-09-30 09:46:24 -0300

    vim: increase conform timeout

--- nvim/lua/plugin-config/conform_config.lua
+++ nvim/lua/plugin-config/conform_config.lua
@@ -47,7 +47,7 @@ local custom_formatters = {
 -- Setup
 conform.setup({
     format_on_save = {
-        timeout_ms = 200,
+        timeout_ms = 500,
         lsp_fallback = false,
     },
     notify_on_error = false,
