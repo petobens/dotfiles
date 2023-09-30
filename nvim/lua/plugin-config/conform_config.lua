656ca66cec07564de2d8fb078524d5826b038e84
petobens <ferrari_pedro@yahoo.com>
2023-09-29 22:26:23 -0300

    vim: add conform sqlfluff and refactor formatter args

--- nvim/lua/plugin-config/conform_config.lua
+++ nvim/lua/plugin-config/conform_config.lua
@@ -10,45 +10,56 @@ require('conform').setup({
         lsp_fallback = false,
     },
     notify_on_error = false,
+    formatters = {
+        sqlfluff = {
+            command = 'sqlfluff',
+            args = {
+                'fix',
+                '--disable-progress-bar',
+                '-f',
+                '-n',
+                '-',
+            },
+            stdin = true,
+        },
+    },
     formatters_by_ft = {
         ['_'] = { 'trim_whitespace' },
         json = { 'jq' },
         lua = { 'stylua' },
         python = { 'isort', 'black' },
         sh = { 'shfmt' },
-        -- TODO: missing sqlfluff
-        -- sql = {}
+        sql = { 'sqlfluff' },
         toml = { 'taplo' },
         yaml = { 'prettierd' },
     },
 })
 
--- Formatter args
-local utils = require('conform.util')
----- Json
-utils.add_formatter_args(require('conform.formatters.jq'), { '--indent', '4' })
----- Lua
-utils.add_formatter_args(
-    require('conform.formatters.stylua'),
+-- Formatters args
+local cu = require('conform.util')
+local formatters = require('conform.formatters')
+--- Json
+cu.add_formatter_args(formatters.jq, { '--indent', '4' })
+--- Lua
+cu.add_formatter_args(
+    formatters.stylua,
     { '--config-path=' .. vim.env.HOME .. '/.config/stylua.toml' }
 )
----- Python
-utils.add_formatter_args(
-    require('conform.formatters.isort'),
+--- Python
+cu.add_formatter_args(
+    formatters.isort,
     { '--settings-file=' .. vim.env.HOME .. '/.isort.cfg' }
 )
-utils.add_formatter_args(
-    require('conform.formatters.black'),
+cu.add_formatter_args(
+    formatters.black,
     { '--config=' .. vim.env.HOME .. '/.config/.black.toml' }
 )
----- sh
-utils.add_formatter_args(require('conform.formatters.shfmt'), { '-i', '4', '-ci', '-sr' })
----- TOML
-require('conform.formatters.taplo').args =
-    { 'format', '--config=' .. vim.env.HOME .. '/taplo.toml', '-' }
+--- sh
+cu.add_formatter_args(formatters.shfmt, { '-i', '4', '-ci', '-sr' })
+--- TOML
+formatters.taplo.args = { 'format', '--config=' .. vim.env.HOME .. '/taplo.toml', '-' }
 
 -- Maps
--- FIXME: Visual formatting doesn't seem to be working
 u.keymap({ 'n', 'v' }, '<Leader>fc', function()
     require('conform').format({ async = true, lsp_fallback = false })
 end)
