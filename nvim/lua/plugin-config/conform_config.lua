e4055543c36ea83ed73a4582c3835e5730b4d0c7
petobens <ferrari_pedro@yahoo.com>
2023-10-16 12:08:40 -0300

    vim: minor conform config refactor

--- nvim/lua/plugin-config/conform_config.lua
+++ nvim/lua/plugin-config/conform_config.lua
@@ -1,21 +1,20 @@
-local u = require('utils')
-
 local conform = require('conform')
-local formatters = require('conform.formatters')
+local u = require('utils')
 
 -- Global options
 vim.o.formatexpr = [[v:lua.require('conform').formatexpr()]]
 
 -- Formatters args
-formatters.jq.args = { '--indent', '4' }
-require('conform').formatters.stylua =
+conform.formatters.jq = { args = { '--indent', '4' } }
+conform.formatters.stylua =
     { prepend_args = { '--config-path=' .. vim.env.HOME .. '/.config/stylua.toml' } }
-require('conform').formatters.isort =
+conform.formatters.isort =
     { prepend_args = { '--settings-file=' .. vim.env.HOME .. '/.isort.cfg' } }
-require('conform').formatters.black =
+conform.formatters.black =
     { prepend_args = { '--config=' .. vim.env.HOME .. '/.config/.black.toml' } }
-require('conform').formatters.shfmt = { prepend_args = { '-i', '4', '-ci', '-sr' } }
-formatters.taplo.args = { 'format', '--config=' .. vim.env.HOME .. '/taplo.toml', '-' }
+conform.formatters.shfmt = { prepend_args = { '-i', '4', '-ci', '-sr' } }
+conform.formatters.taplo =
+    { args = { 'format', '--config=' .. vim.env.HOME .. '/taplo.toml', '-' } }
 
 -- Custom Formatters
 local custom_formatters = {
