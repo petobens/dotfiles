cf5e20f921efbf8a9ce710a9e3dc61fde8493169
petobens <ferrari_pedro@yahoo.com>
2023-10-29 13:20:59 -0300

    vim: auto add markdown list markers when pressing enter

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,6 @@
-vim.opt_local.foldlevel = 1
+vim.opt_local.foldlevel = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.tabstop = 2
+vim.opt_local.formatoptions = 'trjw'
+vim.opt_local.comments = { 'b:*', 'b:-', 'n:>' }
