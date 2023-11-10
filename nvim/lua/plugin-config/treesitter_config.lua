1fe7eff72cc0b55b3dedb4dcc631d03bf9dc15d9
petobens <ferrari_pedro@yahoo.com>
2023-11-10 20:42:06 -0300

    vim: add inspect tree mapping

--- nvim/lua/plugin-config/treesitter_config.lua
+++ nvim/lua/plugin-config/treesitter_config.lua
@@ -73,8 +73,13 @@ u.keymap({ 'n', 'v' }, ']p', '<Cmd>TSTextobjectGotoNextStart @parameter.inner<CR
 u.keymap({ 'n', 'v' }, '[c', '<Cmd>TSTextobjectGotoPreviousStart @class.outer<CR>zz')
 u.keymap({ 'n', 'v' }, '[f', '<Cmd>TSTextobjectGotoPreviousStart @function.outer<CR>zz')
 u.keymap({ 'n', 'v' }, '[p', '<Cmd>TSTextobjectGotoPreviousStart @parameter.inner<CR>zz')
+u.keymap('n', '<Leader>it', function()
+    return vim.treesitter.inspect_tree({
+        command = 'vnew | wincmd H | vertical resize 40',
+    })
+end)
 
--- Custom queries (see for example https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/python/folds.scm)
+-- Custom fold queries (see for example https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/python/folds.scm)
 vim.treesitter.query.set('lua', 'folds', [[[(table_constructor)] @fold]])
 vim.treesitter.query.set('markdown', 'folds', [[[(section)] @fold]])
 vim.treesitter.query.set('python', 'folds', [[[(class_definition)] @fold]])
