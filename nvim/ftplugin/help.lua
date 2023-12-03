1c9121d9cbd38358a2a77364fe63a81ead2b4302
petobens <ferrari_pedro@yahoo.com>
2023-12-03 18:20:15 -0300

    vim: remove help win size cmd

--- nvim/ftplugin/help.lua
+++ nvim/ftplugin/help.lua
@@ -1,9 +1,6 @@
 -- Options
 vim.opt_local.relativenumber = true
-
--- Appearance
-vim.cmd('wincmd J')
-vim.cmd('20 wincmd _')
+vim.cmd('wincmd J') -- show help as bottom window
 
 -- Mappings
 vim.keymap.set('n', 'q', '<Cmd>bdelete<CR>', { buffer = true })
