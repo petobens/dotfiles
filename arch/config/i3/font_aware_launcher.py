49f9156a8e0c2920f8b975d9ecd503f210dd6638
petobens <ferrari_pedro@yahoo.com>
2024-04-03 16:27:30 -0300

    i3: increase htop/ranger sleep hack a bit
    Since active computer is a bit slower

--- arch/config/i3/font_aware_launcher.py
+++ arch/config/i3/font_aware_launcher.py
@@ -74,7 +74,7 @@ APPS = {
     'htop': {
         'type': 'tui',
         # Note: we need the sleep cmd for proper reflow
-        'args': {'title': 'htop', 'cmd': '/usr/bin/bash -c "sleep 0.2&&htop"'},
+        'args': {'title': 'htop', 'cmd': '/usr/bin/bash -c "sleep 0.3&&htop"'},
     },
     'kitty': {
         'type': 'rol_custom',
@@ -172,7 +172,7 @@ APPS = {
         'args': {
             'title': 'ranger',
             # Note: we need the sleep cmd for proper reflow
-            'cmd': '/usr/bin/bash -c "sleep 0.2&&ranger {path}"',
+            'cmd': '/usr/bin/bash -c "sleep 0.3&&ranger {path}"',
         },
     },
     'reboot-dialog': {
