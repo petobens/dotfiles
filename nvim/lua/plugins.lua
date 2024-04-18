836bd23d6318b7cb42cd5acf91145f4979ced828
petobens <ferrari_pedro@yahoo.com>
2024-04-17 21:36:29 -0300

    vim: try neogit plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -315,6 +315,19 @@ local plugins = {
             require('plugin-config.fugitive_config')
         end,
     },
+
+    {
+        'NeogitOrg/neogit',
+        branch = 'nightly',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'sindrets/diffview.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
+        config = function()
+            require('plugin-config.neogit_config')
+        end,
+    },
     ---- Latex
     {
         'lervag/vimtex',
