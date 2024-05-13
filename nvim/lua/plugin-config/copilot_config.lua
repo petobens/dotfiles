5008639b018fe5dcd0b9159ed7a5850c11c1b57e
petobens <ferrari_pedro@yahoo.com>
2024-05-12 23:21:33 -0300

    vim: enable copilot completion for gh actions

--- nvim/lua/plugin-config/copilot_config.lua
+++ nvim/lua/plugin-config/copilot_config.lua
@@ -3,6 +3,7 @@ require('copilot').setup({
     panel = { enabled = false },
     filetypes = {
         ['*'] = false, -- disable for all except explictly defined
+        ghaction = true,
         lua = true,
         python = true,
         sh = true,
