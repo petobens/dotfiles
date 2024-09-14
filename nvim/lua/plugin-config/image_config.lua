1f68c0d0e91fef852a132b6d3efc1792bcb89352
petobens <ferrari_pedro@yahoo.com>
2024-09-14 11:56:38 -0300

    vim: re-add image plugin
    Recover old luarocks discovery code sin lazy's pathspec doesn't seem to
    work

--- nvim/lua/plugin-config/image_config.lua
+++ nvim/lua/plugin-config/image_config.lua
@@ -1,3 +1,13 @@
+--- Ensure magick luarock is loaded
+package.path = package.path
+    .. ';'
+    .. vim.fn.expand('$HOME')
+    .. '/.luarocks/share/lua/5.1/?/init.lua;'
+package.path = package.path
+    .. ';'
+    .. vim.fn.expand('$HOME')
+    .. '/.luarocks/share/lua/5.1/?.lua;'
+
 require('image').setup({
     tmux_show_only_in_active_window = true,
     max_height_window_percentage = 30,
