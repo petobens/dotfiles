efa3b3177c6610dac29ac328f0336cf018e32849
petobens <ferrari_pedro@yahoo.com>
2024-10-28 08:24:51 -0300

    vim: vim.highlight renamed to vim.hl

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -139,7 +139,7 @@ end
 vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
     group = vim.api.nvim_create_augroup('hl_yank', { clear = true }),
     callback = function()
-        vim.highlight.on_yank({ higroup = 'Visual', timeout = 300 })
+        vim.hl.on_yank({ higroup = 'Visual', timeout = 300 })
     end,
 })
 
