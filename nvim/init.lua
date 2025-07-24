a2d1f41e1ad27f4ea3992d8fb80758189923004f
petobens <ferrari_pedro@yahoo.com>
2025-07-24 12:12:32 -0300

    vim: remove custom vim.print override, use default implementation
    This is done natively now

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,29 +8,13 @@ vim.g.do_filetype_lua = true
 vim.env.DOTVIM = vim.env.HOME .. '/.config/nvim'
 vim.env.CACHE = vim.env.DOTVIM .. '/cache/Arch'
 
--- Override some functions
+-- Use silent and nowait by default in mappings
 local keymap_set = vim.keymap.set
 vim.keymap.set = function(mode, lhs, rhs, opts)
     opts = opts or {}
-    -- Use silent and nowait by default in mappings
     opts.silent = opts.silent ~= false
     opts.nowait = opts.nowait ~= false
     return keymap_set(mode, lhs, rhs, opts)
-end
-
-vim.print = function(...)
-    local args = { ... }
-    local msg = table.concat(
-        vim.tbl_map(function(v)
-            return type(v) == 'table' and vim.inspect(v) or tostring(v)
-        end, args),
-        '\t'
-    )
-    -- Always show printed output in the pager
-    vim.schedule(function()
-        vim.api.nvim_echo({ { msg } }, false, { kind = 'list_cmd' })
-    end)
-    return ...
 end
 
 -- Source lua modules
