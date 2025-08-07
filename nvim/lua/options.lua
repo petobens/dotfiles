913be8358287fb7110e0fe1b693575dff730fa90
petobens <ferrari_pedro@yahoo.com>
2025-08-07 14:18:53 -0300

    refactor(options): use method-style option modification and add desc to autocmds
    Replace arithmetic option modifications with :append for iskeyword and foldopen.
    Add descriptive desc fields to all autocmds for improved discoverability and clarity.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,12 +10,8 @@ function _G.my_custom_foldtext()
     return vim.trim(line, marker)
 end
 
--- Syntax
-vim.opt.iskeyword = vim.opt.iskeyword + { ':' }
-vim.opt.termguicolors = true
-
--- Vim behaviour
-require('vim._extui').enable({}) -- experimental new TUI message grid
+-- Core options
+vim.g.editorconfig = false
 vim.opt.autowrite = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.cmdwinheight = 4
@@ -30,41 +26,45 @@ vim.opt.diffopt = {
 }
 vim.opt.foldcolumn = 'auto'
 vim.opt.foldlevelstart = 0
-vim.opt.foldopen = vim.opt.foldopen + { 'insert', 'jump' }
+vim.opt.foldopen:append({ 'insert', 'jump' })
 vim.opt.foldtext = 'v:lua.my_custom_foldtext()'
+vim.opt.iskeyword:append(':')
 vim.opt.lazyredraw = false
 vim.opt.modeline = false
 vim.opt.shortmess = 'aoOtTIcF'
 vim.opt.signcolumn = 'number'
+vim.opt.termguicolors = true
 vim.opt.timeoutlen = 550
 vim.opt.title = true
 vim.opt.ttimeoutlen = 0
 vim.opt.updatetime = 500
 vim.opt.visualbell = true
-vim.g.editorconfig = false
----- Create non-existing parent directory on save
+
+require('vim._extui').enable({}) -- experimental new TUI message grid
+
 vim.api.nvim_create_autocmd('BufWritePre', {
     group = vim.api.nvim_create_augroup('create_dir_before_write', { clear = true }),
+    desc = 'Create parent directory before writing file',
     callback = function()
         u.mk_non_dir()
     end,
 })
----- Save when losing focus
 vim.api.nvim_create_autocmd('FocusLost', {
     group = vim.api.nvim_create_augroup('focus_lost', { clear = true }),
+    desc = 'Save all files when Neovim loses focus',
     callback = function()
         vim.cmd.wall({ mods = { silent = true } })
     end,
 })
----- Disable readonly warning
 vim.api.nvim_create_autocmd('FileChangedRO', {
     group = vim.api.nvim_create_augroup('no_ro_warn', { clear = true }),
+    desc = 'Disable readonly warning after file changed outside of Neovim',
     callback = function()
         vim.opt_local.readonly = false
     end,
 })
 
--- Appearance
+-- UI
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.cursorline = true
 vim.opt.fillchars = {
@@ -89,17 +89,18 @@ vim.opt.startofline = true
 vim.opt.virtualedit = { 'block', 'onemore' }
 vim.opt.winblend = 6
 vim.opt.winborder = 'rounded'
----- Resize splits when the Vim window is resized
+
 vim.api.nvim_create_autocmd('VimResized', {
     group = vim.api.nvim_create_augroup('vim_resized', { clear = true }),
+    desc = 'Equalize all window splits when the Neovim window is resized',
     callback = function()
         vim.cmd.wincmd('=')
     end,
 })
----- Only show cursorline in the current window and save last visited window id
 local cline_acg = vim.api.nvim_create_augroup('cline', { clear = true })
 vim.api.nvim_create_autocmd('WinLeave', {
     group = cline_acg,
+    desc = 'Hide cursorline and save last window id on leaving window',
     callback = function()
         vim.opt_local.cursorline = false
         _G.LastWinId = vim.api.nvim_get_current_win()
@@ -107,28 +108,28 @@ vim.api.nvim_create_autocmd('WinLeave', {
 })
 vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter', 'BufWinEnter' }, {
     group = cline_acg,
+    desc = 'Show cursorline in the active window',
     callback = function()
         vim.opt_local.cursorline = true
     end,
 })
 
--- Backups, sessions, undo and shada
+-- Backups, sessions, undo, shada
 vim.opt.backup = true
 vim.opt.backupdir = vim.fs.joinpath(vim.env.CACHE, 'tmp', 'backup') .. '//'
 vim.opt.directory = vim.fs.joinpath(vim.env.CACHE, 'tmp', 'swap') .. '//'
 vim.opt.sessionoptions = vim.opt.sessionoptions - { 'tabpages' } + { 'winpos', 'resize' }
+vim.opt.shada = [[!,'150,<50,s10,h]]
 vim.opt.shadafile = vim.fs.joinpath(vim.env.CACHE, 'tmp', 'shada', 'main.shada')
 vim.opt.undodir = vim.fs.joinpath(vim.env.CACHE, 'tmp', 'undo') .. '//'
 vim.opt.undofile = true
 vim.opt.viewdir = vim.fs.joinpath(vim.env.CACHE, 'tmp', 'view') .. '//'
-vim.opt.shada = [[!,'150,<50,s10,h]]
----- Save and load viewoptions and previous session
+
 local session_acg = vim.api.nvim_create_augroup('session', { clear = true })
 vim.api.nvim_create_autocmd('VimLeavePre', {
     group = session_acg,
+    desc = 'Save session on exit if UI is attached',
     callback = function()
-        -- Only save session if there is an attached UI (i.e. not in headless mode such
-        -- as a neotest worker)
         if #vim.api.nvim_list_uis() > 0 then
             vim.cmd.mksession({ args = { u.vim_session_file() }, bang = true })
         end
@@ -138,6 +139,7 @@ local session_patterns = { '*.*', 'bashrc', 'bash_profile', 'config' }
 vim.api.nvim_create_autocmd('BufWinLeave', {
     group = session_acg,
     pattern = session_patterns,
+    desc = 'Save view when leaving buffer window',
     callback = function(args)
         if not vim.wo.previewwindow then
             vim.api.nvim_buf_call(args.buf, function()
@@ -149,6 +151,7 @@ vim.api.nvim_create_autocmd('BufWinLeave', {
 vim.api.nvim_create_autocmd('BufWinEnter', {
     group = session_acg,
     pattern = session_patterns,
+    desc = 'Restore view when entering buffer window',
     callback = function(args)
         if not vim.wo.previewwindow then
             vim.api.nvim_buf_call(args.buf, function()
@@ -158,26 +161,26 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
     end,
 })
 
--- Search, matching, substitution and yanking
+-- Search, matching, substitution, yanking
 vim.opt.gdefault = true
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.matchtime = 1
 vim.opt.showmatch = true
 vim.opt.smartcase = true
-if vim.fn.executable('rg') then
+if vim.fn.executable('rg') == 1 then
     vim.opt.grepprg = 'rg --smart-case --vimgrep --no-heading'
     vim.opt.grepformat = { '%f:%l:%c:%m', '%f:%l:%m' }
 end
----- Briefly highlight yanked text
-vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+vim.api.nvim_create_autocmd('TextYankPost', {
     group = vim.api.nvim_create_augroup('hl_yank', { clear = true }),
+    desc = 'Briefly highlight yanked text',
     callback = function()
         vim.hl.on_yank({ higroup = 'Visual', timeout = 300 })
     end,
 })
 
--- Editing, tab and indent
+-- Editing, tabs, indent
 vim.opt.autoindent = true
 vim.opt.breakindent = true
 vim.opt.colorcolumn = { '+1' }
@@ -218,7 +221,7 @@ vim.opt.wildignore = {
 vim.opt.wildignorecase = true
 vim.opt.wildmode = { 'longest:full', 'full' }
 
--- Misc
+-- Spelling
 vim.opt.spellfile =
     vim.fs.joinpath(vim.env.DOTVIM, 'spell', 'custom-dictionary.utf-8.add')
 vim.opt.spelllang = { 'en', 'es' }
@@ -249,12 +252,13 @@ vim.filetype.add({
     },
 })
 
--- Filetype options
+-- Filetype-specific options
 local ftoptions_group = vim.api.nvim_create_augroup('FtOptions', {})
 local function set_ft_option(ft, vim_cmd)
     vim.api.nvim_create_autocmd('FileType', {
         pattern = ft,
         group = ftoptions_group,
+        desc = ('Set filetype-specific options: %s'):format(vim.inspect(ft)),
         command = vim_cmd,
     })
 end
@@ -263,11 +267,13 @@ set_ft_option({ 'html' }, 'setlocal shiftwidth=2 tabstop=2 softtabstop=2')
 set_ft_option({ 'i3config', 'sh' }, 'setlocal foldmethod=marker')
 set_ft_option({ 'text' }, 'setlocal shiftwidth=2 tabstop=2 softtabstop=2 spell')
 set_ft_option({ 'vim' }, 'setlocal foldmethod=marker formatoptions-=ro')
----- Python
+
+-- Python: auto-activate virtualenvs
 _G.PyVenv = { active_venv = {} } -- if set inside ftplugin file it will be reset
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+vim.api.nvim_create_autocmd('BufEnter', {
     group = vim.api.nvim_create_augroup('auto_venv', { clear = true }),
     pattern = { '*.py' },
+    desc = 'Auto-activate Python virtualenv on entering Python buffer',
     callback = function()
         local fname = vim.api.nvim_buf_get_name(0)
         if not string.match(fname, '.git/') and not vim.startswith(fname, 'copilot') then
