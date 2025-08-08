ce309262399f034f6fd10a2bd7e6eb9fecbfef2f
petobens <ferrari_pedro@yahoo.com>
2025-08-08 19:23:36 -0300

    refactor(nvim): use function-style vim.cmd and add desc to fugitive keymaps
    Modernize fugitive config by replacing string-based Ex commands with
    function-style vim.cmd calls and adding descriptive `desc` fields to all
    keymaps and autocmds for improved discoverability and maintainability.

--- nvim/lua/plugin-config/fugitive_config.lua
+++ nvim/lua/plugin-config/fugitive_config.lua
@@ -4,28 +4,48 @@ _G.fugitiveConfig = {}
 
 -- Autocmds
 vim.api.nvim_create_autocmd('FileType', {
+    desc = 'Set up Fugitive git status options and mappings',
     group = vim.api.nvim_create_augroup('ps_fugitive', { clear = true }),
-    pattern = { 'fugitive' }, -- gstatus
+    pattern = { 'fugitive' },
     callback = function(e)
         -- Options
         vim.opt_local.winfixheight = true
         vim.opt_local.winfixbuf = true
+
         -- Mappings
-        vim.keymap.set('n', 'q', u.quit_return, { buffer = e.buf })
-        vim.keymap.set('n', ']h', ']c', { buffer = e.buf, remap = true })
-        vim.keymap.set('n', '[h', '[c', { buffer = e.buf, remap = true })
-        vim.keymap.set('n', 'ci', '<Cmd>Git commit -n<CR>', { buffer = e.buf })
-        vim.keymap.set('n', '<Leader>gp', '<Cmd>Git push<CR>', { buffer = e.buf })
         vim.keymap.set(
             'n',
-            '<Leader>gF',
-            '<Cmd>Git push --force-with-lease<CR>',
-            { buffer = e.buf }
+            'q',
+            u.quit_return,
+            { buffer = e.buf, desc = 'Quit Fugitive and return' }
         )
-        vim.keymap.set('n', '<Leader>gP', '<Cmd>Git pull<CR>', { buffer = e.buf })
+        vim.keymap.set(
+            'n',
+            ']h',
+            ']c',
+            { buffer = e.buf, remap = true, desc = 'Next hunk' }
+        )
+        vim.keymap.set(
+            'n',
+            '[h',
+            '[c',
+            { buffer = e.buf, remap = true, desc = 'Previous hunk' }
+        )
+        vim.keymap.set('n', 'ci', function()
+            vim.cmd.Git({ args = { 'commit', '-n' } })
+        end, { buffer = e.buf, desc = 'Commit ignore check (no verify)' })
+        vim.keymap.set('n', '<Leader>gp', function()
+            vim.cmd.Git({ args = { 'push' } })
+        end, { buffer = e.buf, desc = 'Push' })
+        vim.keymap.set('n', '<Leader>gF', function()
+            vim.cmd.Git({ args = { 'push', '--force-with-lease' } })
+        end, { buffer = e.buf, desc = 'Force push with lease' })
+        vim.keymap.set('n', '<Leader>gP', function()
+            vim.cmd.Git({ args = { 'pull' } })
+        end, { buffer = e.buf, desc = 'Pull' })
         vim.keymap.set('n', '<Leader>gl', function()
             vim.cmd.Git({ args = { 'log', '--oneline' } })
-        end, { buffer = e.buf })
+        end, { buffer = e.buf, desc = 'Git log (oneline)' })
         vim.keymap.set('n', '<Leader>nd', function()
             local file = vim.fn.FugitiveFind(vim.fn.expand('<cfile>'))
             if not file or file == '' or not vim.uv.fs_stat(file) then
@@ -40,37 +60,50 @@ vim.api.nvim_create_autocmd('FileType', {
                 vim.log.levels.INFO
             )
             vim.system({ 'nbdiff-web', 'HEAD', file }, { detach = true })
-        end, { buffer = e.buf })
+        end, { buffer = e.buf, desc = 'Open nbdiff-web for file under cursor' })
     end,
 })
+
 vim.api.nvim_create_autocmd('FileType', {
+    desc = 'Set up git filetype (diffs & commit history) options and mappings',
     group = vim.api.nvim_create_augroup('git_ft', { clear = true }),
-    pattern = { 'git' }, -- basically diffs/commit history
+    pattern = { 'git' },
     callback = function(e)
         vim.opt_local.foldlevel = 1 -- open commits unfolded
-        vim.keymap.set('n', 'q', u.quit_return, { buffer = e.buf })
+        vim.keymap.set(
+            'n',
+            'q',
+            u.quit_return,
+            { buffer = e.buf, desc = 'Quit and return' }
+        )
     end,
 })
+
 vim.api.nvim_create_autocmd('FileType', {
+    desc = 'Set up gitcommit filetype options and mappings',
     group = vim.api.nvim_create_augroup('git_commit_ft', { clear = true }),
     pattern = { 'gitcommit' },
     callback = function(e)
         -- Options
         vim.opt_local.spell = true
+
         -- Mappings
         vim.keymap.set('n', '<Leader>ac', function()
             vim.cmd.normal({ args = { 'gg0' }, bang = true })
             vim.cmd.normal({ args = { 'dd' }, bang = true })
             vim.cmd.update({ mods = { silent = true, noautocmd = true } })
             vim.cmd.bd()
-        end, { buffer = e.buf })
+        end, { buffer = e.buf, desc = 'Abort commit' })
     end,
 })
+
+local git_commit_edit = vim.api.nvim_create_augroup('git_commit_edit', { clear = true })
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-    group = vim.api.nvim_create_augroup('git_commit_insert', { clear = true }),
+    desc = 'Startinsert and highlight commit message summary',
+    group = git_commit_edit,
     pattern = { '*.git/COMMIT_EDITMSG' },
     callback = function()
-        vim.cmd.wincmd({ args = { '15_' } })
+        vim.cmd.wincmd('15_')
         vim.cmd.normal({ args = { 'gg0' }, bang = true })
         if vim.api.nvim_get_current_line() == '' then
             vim.cmd.startinsert()
@@ -82,7 +115,8 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     end,
 })
 vim.api.nvim_create_autocmd({ 'BufLeave' }, {
-    group = vim.api.nvim_create_augroup('git_commit_leave', { clear = true }),
+    desc = 'Go back to git status window after leaving commit message buffer',
+    group = git_commit_edit,
     pattern = { '*.git/COMMIT_EDITMSG' },
     callback = function()
         vim.api.nvim_set_current_win(_G.fugitiveConfig.gstatus_winid)
@@ -91,29 +125,49 @@ vim.api.nvim_create_autocmd({ 'BufLeave' }, {
 
 -- Mappings
 vim.keymap.set('n', '<Leader>gs', function()
-    vim.cmd.lcd({ args = { vim.fs.dirname(vim.api.nvim_buf_get_name(0)) } })
+    vim.cmd.lcd(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
     vim.cmd('botright Git')
-    vim.cmd.wincmd({ args = { 'J' } })
-    vim.cmd.resize({ args = { '15' } })
+    vim.cmd.wincmd('J')
+    vim.cmd.resize('15')
     vim.cmd.normal({ args = { '4j' }, bang = true })
     _G.fugitiveConfig.gstatus_winid = vim.api.nvim_get_current_win()
-end)
-vim.keymap.set('n', '<Leader>gd', '<Cmd>Gdiffsplit!<CR>')
-vim.keymap.set('n', '<Leader>gD', ':Git diff<space>', { silent = false })
-vim.keymap.set('n', '<Leader>gM', '<Cmd>Git! mergetool<CR>')
-vim.keymap.set('n', '<Leader>gr', ':Git rebase -i<space>', { silent = false })
-vim.keymap.set('n', '<Leader>gp', '<Cmd>lcd %:p:h<CR><Cmd>Git push<CR>')
+end, { desc = 'Open Fugitive status window' })
+vim.keymap.set('n', '<Leader>gd', function()
+    vim.cmd.Gdiffsplit({ bang = true })
+end, { desc = 'Run Gdiffsplit' })
+vim.keymap.set('n', '<Leader>gD', function()
+    vim.cmd.Git({ args = { 'diff' } })
+end, { desc = 'Git diff prompt' })
+vim.keymap.set('n', '<Leader>gM', function()
+    vim.cmd.Git({ args = { 'mergetool' }, bang = true })
+end, { desc = 'Git mergetool' })
+vim.keymap.set('n', '<Leader>gr', function()
+    vim.cmd.Git({ args = { 'rebase', '-i' } })
+end, { desc = 'Git interactive rebase' })
+vim.keymap.set('n', '<Leader>gp', function()
+    vim.cmd.lcd({ args = { vim.fs.dirname(vim.api.nvim_buf_get_name(0)) } })
+    vim.cmd.Git({ args = { 'push' } })
+end, { desc = 'Push' })
+vim.keymap.set('n', '<Leader>gF', function()
+    vim.cmd.lcd({ args = { vim.fs.dirname(vim.api.nvim_buf_get_name(0)) } })
+    vim.cmd.Git({ args = { 'push', '--force-with-lease' } })
+end, { desc = 'Force push with lease' })
+vim.keymap.set('n', '<Leader>gP', function()
+    vim.cmd.lcd({ args = { vim.fs.dirname(vim.api.nvim_buf_get_name(0)) } })
+    vim.cmd.Git({ args = { 'pull' } })
+end, { desc = 'Pull' })
 vim.keymap.set(
-    'n',
-    '<Leader>gF',
-    '<Cmd>lcd %:p:h<CR><Cmd>Git push --force-with-lease<CR>'
+    { 'n', 'v' },
+    '<Leader>gb',
+    vim.cmd.GBrowse,
+    { desc = 'Browse git object' }
 )
-vim.keymap.set('n', '<Leader>gP', '<Cmd>lcd %:p:h<CR><Cmd>Git pull<CR>')
-vim.keymap.set({ 'n', 'v' }, '<Leader>gb', ':GBrowse<CR>')
-vim.keymap.set({ 'n', 'v' }, '<Leader>gB', ':GBrowse!<CR>')
+vim.keymap.set({ 'n', 'v' }, '<Leader>gB', function()
+    vim.cmd.GBrowse({ bang = true })
+end, { desc = 'Copy browser permalink' })
 vim.keymap.set('n', '<Leader>bl', function()
     vim.cmd.Git({ args = { 'blame' }, range = { 0, 3 } })
-    vim.cmd.wincmd({ args = { 'j' } })
+    vim.cmd.wincmd('j')
     vim.cmd.normal({ args = { '5j' }, bang = true })
-    vim.cmd.wincmd({ args = { '25_' } })
-end)
+    vim.cmd.wincmd('25_')
+end, { desc = 'Open git blame and adjust window layout' })
