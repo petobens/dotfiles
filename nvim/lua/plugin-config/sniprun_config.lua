4baa7e8dcacf55c27f6a8528f8daddf7660161d8
petobens <ferrari_pedro@yahoo.com>
2025-08-09 17:35:06 -0300

    refactor(nvim): add desc to SnipRun keymaps for clarity

--- nvim/lua/plugin-config/sniprun_config.lua
+++ nvim/lua/plugin-config/sniprun_config.lua
@@ -1,5 +1,20 @@
 require('sniprun').setup({})
 
-vim.keymap.set({ 'n', 'v' }, '<Leader>br', '<Plug>SnipRun')
-vim.keymap.set('n', '<Leader>bc', '<Plug>SnipClose')
-vim.keymap.set('n', '<Leader>bw', '<Plug>SnipReset')
+vim.keymap.set(
+    { 'n', 'v' },
+    '<Leader>br',
+    '<Plug>SnipRun',
+    { desc = 'Run block code with SnipRun' }
+)
+vim.keymap.set(
+    'n',
+    '<Leader>bc',
+    '<Plug>SnipClose',
+    { desc = 'Close SnipRun block virtual text' }
+)
+vim.keymap.set(
+    'n',
+    '<Leader>bw',
+    '<Plug>SnipReset',
+    { desc = 'Reset/Wipe SnipRun block' }
+)
