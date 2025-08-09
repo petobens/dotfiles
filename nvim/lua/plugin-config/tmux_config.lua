195f5247be63542df4392dcb97106061171e7e4c
petobens <ferrari_pedro@yahoo.com>
2025-08-09 17:36:44 -0300

    refactor(nvim): add desc to tmux navigation keymaps for clarity

--- nvim/lua/plugin-config/tmux_config.lua
+++ nvim/lua/plugin-config/tmux_config.lua
@@ -1,6 +1,6 @@
 local tmux_plugin = require('tmux')
 
-vim.keymap.set('n', '<C-h>', tmux_plugin.move_left)
-vim.keymap.set('n', '<C-j>', tmux_plugin.move_down)
-vim.keymap.set('n', '<C-k>', tmux_plugin.move_up)
-vim.keymap.set('n', '<C-l>', tmux_plugin.move_right)
+vim.keymap.set('n', '<C-h>', tmux_plugin.move_left, { desc = 'Tmux: Move left' })
+vim.keymap.set('n', '<C-j>', tmux_plugin.move_down, { desc = 'Tmux: Move down' })
+vim.keymap.set('n', '<C-k>', tmux_plugin.move_up, { desc = 'Tmux: Move up' })
+vim.keymap.set('n', '<C-l>', tmux_plugin.move_right, { desc = 'Tmux: Move right' })
