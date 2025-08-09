28c373c3022a05bb96312a5f100c0f90db182008
petobens <ferrari_pedro@yahoo.com>
2025-08-09 10:37:04 -0300

    refactor(invim): use function-style vim.cmd and add desc to keymap in indnentline config

--- nvim/lua/plugin-config/indentlines_config.lua
+++ nvim/lua/plugin-config/indentlines_config.lua
@@ -1,11 +1,10 @@
 local hooks = require('ibl.hooks')
-local ibl = require('ibl')
 
-ibl.setup({
+require('ibl').setup({
     enabled = false,
     indent = { char = '|' },
     scope = { enabled = true, char = '│', show_start = false, show_end = false },
 })
 hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
 
-vim.keymap.set('n', '<Leader>I', '<Cmd>IBLToggle<CR>')
+vim.keymap.set('n', '<Leader>I', vim.cmd.IBLToggle, { desc = 'Toggle indent lines' })
