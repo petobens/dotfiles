4ff69deb1ec9f5c10977b57f68ff56e8fa6ae5a3
petobens <ferrari_pedro@yahoo.com>
2025-08-09 10:28:28 -0300

    refactor(nvim): add desc to gitsigns keymaps for clarity and consistency

--- nvim/lua/plugin-config/gitsigns_config.lua
+++ nvim/lua/plugin-config/gitsigns_config.lua
@@ -1,5 +1,6 @@
 local gitsigns = require('gitsigns')
 
+-- Setup
 gitsigns.setup({
     signcolumn = false, -- disable by default
     watch_gitdir = {
@@ -11,7 +12,7 @@ gitsigns.setup({
 })
 
 -- Mappings
-vim.keymap.set('n', '<Leader>gg', gitsigns.toggle_signs)
+vim.keymap.set('n', '<Leader>gg', gitsigns.toggle_signs, { desc = 'Toggle gitsigns' })
 vim.keymap.set('n', ']h', function()
     if vim.wo.diff then
         return ']c'
@@ -20,8 +21,7 @@ vim.keymap.set('n', ']h', function()
         gitsigns.nav_hunk('next', { navigation_message = false, foldopen = true })
     end)
     return '<Ignore>'
-end, { expr = true })
-
+end, { expr = true, desc = 'Next git hunk' })
 vim.keymap.set('n', '[h', function()
     if vim.wo.diff then
         return '[c'
@@ -30,9 +30,14 @@ vim.keymap.set('n', '[h', function()
         gitsigns.nav_hunk('prev', { navigation_message = false, foldopen = true })
     end)
     return '<Ignore>'
-end, { expr = true })
-vim.keymap.set('n', '<Leader>hp', gitsigns.preview_hunk)
+end, { expr = true, desc = 'Previous git hunk' })
+vim.keymap.set('n', '<Leader>hp', gitsigns.preview_hunk, { desc = 'Preview git hunk' })
 vim.keymap.set('n', '<Leader>gm', function()
     gitsigns.blame_line({ full = true })
-end)
-vim.keymap.set('n', '<Leader>ib', gitsigns.toggle_current_line_blame)
+end, { desc = 'Show git blame for current line' })
+vim.keymap.set(
+    'n',
+    '<Leader>ib',
+    gitsigns.toggle_current_line_blame,
+    { desc = 'Toggle inline git blame' }
+)
