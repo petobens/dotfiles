3f5480a43d24245131d16ab1328e03570eb8bb64
petobens <ferrari_pedro@yahoo.com>
2025-08-13 14:20:19 -0300

    refactor(nvim): modernize qf ftplugin and add desc to keymaps/autocmds
    Refactor quickfix/loclist ftplugin to use function-style vim.cmd, add
    descriptions to all keymaps and autocmds, and improve window handling logic.

--- nvim/ftplugin/qf.lua
+++ nvim/ftplugin/qf.lua
@@ -4,48 +4,60 @@ vim.opt_local.textwidth = 0
 vim.opt_local.spell = false
 vim.opt_local.buflisted = false
 vim.opt_local.winfixbuf = true
+-- Show qf as bottom window
+vim.cmd.wincmd('J')
+vim.api.nvim_win_set_height(0, math.max(1, math.min(vim.api.nvim_buf_line_count(0), 15)))
 
--- Appearance
-vim.cmd.wincmd({ args = { 'J' } })
-vim.cmd(math.max(1, math.min(vim.fn.line('$'), 15)) .. 'wincmd _')
-
--- Mappings
-local map_opts = { buffer = true }
-vim.keymap.set('n', 'q', function()
-    local close_cmd = 'cclose'
-    if vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0 then
-        close_cmd = 'lclose'
-    end
-    vim.api.nvim_set_current_win(_G.LastWinId)
-    if close_cmd == 'cclose' then
-        vim.cmd.cclose()
-    else
-        vim.cmd.lclose()
-    end
-end, map_opts)
-vim.keymap.set('n', 'Q', 'q', { buffer = true, remap = true })
-vim.keymap.set('n', '<C-s>', '<C-w><Enter>', map_opts)
-vim.keymap.set('n', '<C-v>', '<C-w><Enter><C-w>L', map_opts)
-vim.keymap.set(
-    'n',
-    '<C-q>',
-    '<Cmd>cclose<bar>wincmd p<bar>Telescope quickfix<CR>',
-    map_opts
-)
-vim.keymap.set(
-    'n',
-    '<C-l>',
-    '<Cmd>lclose<bar>wincmd p<bar>Telescope loclist<CR>',
-    map_opts
-)
-
--- Autocmds
+-- Autocmd options
 vim.api.nvim_create_autocmd({ 'QuitPre', 'BufDelete' }, {
     group = vim.api.nvim_create_augroup('ft_qf', { clear = true }),
+    desc = 'Auto-close loclist when quitting a window',
     callback = function()
-        -- Automatically close corresponding loclist when quitting a window
         if vim.bo.filetype ~= 'qf' then
             vim.cmd.lclose({ mods = { silent = true } })
         end
     end,
 })
+
+-- Mappings
+vim.keymap.set('n', 'q', function()
+    local close_cmd = vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0 and 'lclose'
+        or 'cclose'
+    if _G.LastWinId and vim.api.nvim_win_is_valid(_G.LastWinId) then
+        vim.api.nvim_set_current_win(_G.LastWinId)
+    end
+    vim.cmd[close_cmd]()
+end, { buffer = true, desc = 'Close quickfix/loclist and return to last window' })
+
+vim.keymap.set(
+    'n',
+    'Q',
+    'q',
+    { buffer = true, remap = true, desc = "Alias for 'q' (close qf/loclist and return)" }
+)
+
+vim.keymap.set(
+    'n',
+    '<C-s>',
+    '<C-w><Enter>',
+    { buffer = true, desc = 'Open entry in split' }
+)
+
+vim.keymap.set(
+    'n',
+    '<C-v>',
+    '<C-w><Enter><C-w>L',
+    { buffer = true, desc = 'Open entry in vsplit' }
+)
+
+vim.keymap.set('n', '<C-q>', function()
+    vim.cmd.cclose()
+    vim.cmd.wincmd('p')
+    vim.cmd.Telescope('quickfix')
+end, { buffer = true, desc = 'Close quickfix and open Telescope quickfix' })
+
+vim.keymap.set('n', '<C-l>', function()
+    vim.cmd.lclose()
+    vim.cmd.wincmd('p')
+    vim.cmd.Telescope('loclist')
+end, { buffer = true, desc = 'Close loclist and open Telescope loclist' })
