f5218c5d2b8fe3318bcadbc9ca997af70092ca5d
petobens <ferrari_pedro@yahoo.com>
2025-08-12 23:17:09 -0300

    refactor(nvim): add desc to help buffer keymaps for clarity

--- nvim/ftplugin/help.lua
+++ nvim/ftplugin/help.lua
@@ -6,5 +6,10 @@ vim.api.nvim_win_set_height(0, 20)
 vim.api.nvim_win_set_option(0, 'winfixheight', true)
 
 -- Mappings
-vim.keymap.set('n', 'q', '<Cmd>bdelete<CR>', { buffer = true })
-vim.keymap.set('n', '<Leader>tc', 'gO', { buffer = true, remap = true })
+vim.keymap.set('n', 'q', vim.cmd.bdelete, { buffer = true, desc = 'Close help buffer' })
+vim.keymap.set(
+    'n',
+    '<Leader>tc',
+    'gO',
+    { buffer = true, remap = true, desc = 'Go to help table of contents' }
+)
