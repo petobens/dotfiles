0b098da7f97cd2d519679160388e25870eedadda
petobens <ferrari_pedro@yahoo.com>
2026-01-20 20:23:05 -0300

    fix(polybar): improve bluetooth module detection and add blacklist

--- arch/config/polybar/bluetooth.sh
+++ arch/config/polybar/bluetooth.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
 
-bluetooth_status=$(bluetoothctl show | grep "Powered:" | awk '{print $2}')
-if [ "$bluetooth_status" == "yes" ]; then
-    connected_device=$(bluetoothctl info | grep "Connected:" | awk '{print $2}')
-    if [ "$connected_device" == "yes" ]; then
-        echo -n '󰂯 '
-    else
-        echo -n '󰂲 '
-    fi
-else
+bluetooth_status="$(
+    bluetoothctl show 2> /dev/null | awk '/Powered:/ {print $2; exit}'
+)"
+
+BLACKLIST_NAMES=(
+    "MX Master 3S"
+)
+
+is_blacklisted() {
+    local name_lc=${1,,} b
+    for b in "${BLACKLIST_NAMES[@]}"; do
+        [[ $name_lc == *"${b,,}"* ]] && return 0
+    done
+    return 1
+}
+
+[[ $bluetooth_status == "yes" ]] || {
     echo -n '  '
+    exit 0
+}
+
+connected_device="no"
+while IFS= read -r line; do
+    [[ $line == Device\ * ]] || continue
+
+    name="${line#Device }"
+    name="${name#* }"
+
+    is_blacklisted "$name" && continue
+    connected_device="yes"
+    break
+done < <(bluetoothctl devices Connected 2> /dev/null)
+
+if [[ $connected_device == "yes" ]]; then
+    echo -n '󰂯 '
+else
+    echo -n '󰂲 '
 fi
