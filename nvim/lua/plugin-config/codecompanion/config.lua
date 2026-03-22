2630f2329d704709b1eb3e4596c61812ca0867b3
petobens <ferrari_pedro@yahoo.com>
2026-03-22 10:49:53 -0300

    refactor(nvim): move CodeCompanion extensions into module

--- nvim/lua/plugin-config/codecompanion/config.lua
+++ nvim/lua/plugin-config/codecompanion/config.lua
@@ -1,8 +1,8 @@
 -- luacheck:ignore 631
 local codecompanion = require('codecompanion')
-local u = require('utils')
 
 local adapter_config = require('plugin-config.codecompanion.adapters')
+local extensions = require('plugin-config.codecompanion.extensions')
 local mappings = require('plugin-config.codecompanion.mappings')
 local mcp = require('plugin-config.codecompanion.mcp')
 local prompt_library = require('plugin-config.codecompanion.prompt_library')
@@ -117,28 +117,7 @@ function M.setup()
         -- MCP
         mcp = mcp.build(),
         -- Extensions
-        extensions = {
-            history = {
-                enabled = true,
-                opts = {
-                    auto_generate_title = u.is_online(),
-                    title_generation_opts = {
-                        adapter = 'openai_gpt_54_nano_legacy',
-                        model = 'gpt-5.4-nano',
-                        refresh_every_n_prompts = 3,
-                        max_refreshes = 10,
-                    },
-                    auto_save = true,
-                    expiration_days = 30,
-                    keymap = { n = '<A-s>', i = '<A-s>' },
-                    picker_keymaps = {
-                        rename = { n = 'r', i = '<A-r>' },
-                        delete = { n = 'd', i = '<A-d>' },
-                    },
-                    save_chat_keymap = { n = '<nop>', i = '<nop>' },
-                },
-            },
-        },
+        extensions = extensions.build(),
     })
 end
 
