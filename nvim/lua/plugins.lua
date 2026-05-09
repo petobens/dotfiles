05fa64a74502d9fd2395a296c1bcdff31ca3b4bb
petobens <ferrari_pedro@yahoo.com>
2026-05-09 08:26:34 -0300

    chore(nvim): lower blink.cmp build wait timeout

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -109,7 +109,7 @@ local plugins = {
             { 'MattiasMTS/cmp-dbee', branch = 'ms/v2' },
         },
         build = function()
-            require('blink.cmp').build():wait(20000)
+            require('blink.cmp').build():wait(15000)
         end,
         event = 'InsertEnter',
         config = load_plugin_config('blink_cmp_config'),
