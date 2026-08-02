e996a3a6cf5e93e47a284bfa6004936c432fe66f
petobens <ferrari_pedro@yahoo.com>
2026-08-02 09:22:36 -0300

    feat(fish): show vi modes with distinct cursor shapes
    Use block, line, and underscore cursors for normal, insert, and replace-one
    modes, respectively.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -26,6 +26,11 @@ if status is-interactive
 end
 
 # Key bindings
+set -g fish_vi_force_cursor 1
+set -g fish_cursor_default block
+set -g fish_cursor_insert line
+set -g fish_cursor_replace_one underscore
+
 function __set_vi_key_bindings
     set -g fish_key_bindings fish_vi_key_bindings
     bind -M default \ce edit_command_buffer
