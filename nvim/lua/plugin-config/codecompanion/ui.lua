d8488700d9d9537c197530a94a129462984732c1
petobens <ferrari_pedro@yahoo.com>
2026-08-04 14:13:19 -0300

    fix(nvim): widen CodeCompanion chat window

--- nvim/lua/plugin-config/codecompanion/ui.lua
+++ nvim/lua/plugin-config/codecompanion/ui.lua
@@ -310,7 +310,7 @@ function M.chat_display()
             layout = 'float',
             border = 'rounded',
             height = vim.o.lines - 5,
-            width = 0.461,
+            width = 0.495,
             relative = 'editor',
             col = vim.o.columns,
             row = 1,
