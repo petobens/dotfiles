df058274018136cfb2744de989465bb5056f83e6
petobens <ferrari_pedro@yahoo.com>
2026-09-01 13:55:19 -0300

    feat(nvim): support MyST Markdown highlighting and matching
    Highlight colon containers, directive options, and equation references that
    the Markdown parser does not recognize. Extend matchup motions to handle
    containers, labeled equations, and code fences.

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -6,3 +6,36 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.formatoptions = 'trjw'
 vim.opt_local.comments = {}
+
+-- Highlight containers, options, and equation roles missing from the parser
+local namespace = vim.api.nvim_create_namespace('markdown_extensions')
+
+local function highlight_range(bufnr, row, group, start_col, end_col)
+    vim.hl.range(bufnr, namespace, group, { row, start_col }, { row, end_col })
+end
+
+local function highlight_extensions(args)
+    local bufnr = args.buf
+    vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+    for row, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)) do
+        local indent, token = line:match('^(%s*)(:::+.*)$')
+        local group = 'PreProc'
+        if not token then
+            indent, token = line:match('^(%s*)(:[%w_-]+:)')
+            group = '@property'
+        end
+        if token then
+            highlight_range(bufnr, row - 1, group, #indent, #indent + #token)
+        end
+
+        for first, last in line:gmatch('(){eq}`[^`]+`()') do
+            highlight_range(bufnr, row - 1, 'Number', first - 1, last - 1)
+        end
+    end
+end
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'TextChanged', 'TextChangedI' }, {
+    buffer = 0,
+    desc = 'Highlight Markdown extension syntax',
+    callback = highlight_extensions,
+})
--- nvim/lua/plugin-config/matchup_config.lua
+++ nvim/lua/plugin-config/matchup_config.lua
@@ -1,3 +1,28 @@
+-- Helpers
+local markdown_match_words = table.concat({
+    -- Colon containers
+    [=[^\s*\zs\:\{3}\:*\ze\%(\s\+\S\|{\):^\s*\zs\:\{3}\:*\ze\s*$]=],
+    -- MyST display math with a closing equation label
+    [=[^\s*\zs\$\$\ze\s*$:\$\$\ze\s\+(eq\:[^)]\+)\s*$]=],
+    -- Display math with content on the opening line
+    [=[^\s*\zs\$\$\ze\S:\$\$\ze\s*$]=],
+    -- Labeled code fences
+    [=[^\s*\zs`\{3}`*\ze\%(\s\+\S\|\w\):^\s*\zs`\{3}`*\ze\s*$]=],
+    [=[^\s*\zs\~\{3}\~*\ze\%(\s\+\S\|\w\):^\s*\zs\~\{3}\~*\ze\s*$]=],
+}, ',')
+
+local function configure_markdown(args)
+    if vim.bo[args.buf].filetype ~= 'markdown' then
+        return
+    end
+    vim.b[args.buf].matchup_treesitter_enabled = false
+    vim.b[args.buf].match_words = markdown_match_words
+    vim.api.nvim_buf_call(args.buf, function()
+        vim.fn['matchup#loader#init_buffer']()
+    end)
+end
+
+-- Mappings
 vim.keymap.set(
     { 'n', 'v', 'o' },
     '<tab>',
@@ -10,3 +35,9 @@ vim.keymap.set(
     'g%',
     { remap = true, desc = 'Jump to previous matching pair' }
 )
+
+-- Autocmds
+vim.api.nvim_create_autocmd('BufEnter', {
+    desc = 'Matchup: configure Markdown delimiters',
+    callback = configure_markdown,
+})
