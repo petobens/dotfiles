11d955623e5183d411fbf289e75e9aa177506a2c
petobens <ferrari_pedro@yahoo.com>
2026-09-03 09:00:10 -0300

    style(hypr): improve active window border visibility

--- hypr/conf/options.lua
+++ hypr/conf/options.lua
@@ -10,7 +10,7 @@ hl.config({
         gaps_out = 4,
         border_size = 1,
         col = {
-            active_border = '#3b3b3b',
+            active_border = '#424242',
             inactive_border = '#282c34',
         },
         resize_on_border = true,
