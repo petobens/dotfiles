43f5cb1c1fcc21c15ae08566c7ee5ad7e178b917
petobens <ferrari_pedro@yahoo.com>
2026-09-03 16:21:53 -0300

    refactor(yazi): organize initialization by responsibility
    Group plugin setup, interface overrides, line mode, and bookmarks into
    clearly labeled sections without changing behavior.

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,10 +1,12 @@
 -- luacheck: globals App Header Linemode Status th ui ya
 
+-- Plugins
 require('full-border'):setup({ type = ui.Border.THICK })
 require('git'):setup()
 require('folder-rules'):setup()
 require('toggle-pane'):entry('max-current')
 
+-- Header
 -- Keep the folder icon visible when long paths are truncated
 function Header:cwd()
     local max = self._area.w - self._right_width - 3
@@ -17,6 +19,7 @@ function Header:cwd()
         :style(th.mgr.cwd)
 end
 
+-- Status line
 -- Keep the position neutral instead of repeating the current mode color
 function Status:position()
     local cursor = self._current.cursor
@@ -32,6 +35,22 @@ function Status:position()
     })
 end
 
+-- File list
+function Linemode:size_and_mtime()
+    local time = math.floor(self._file.cha.mtime or 0)
+    if time == 0 then
+        time = ''
+    elseif os.date('%Y', time) == os.date('%Y') then
+        time = os.date('%b %d %H:%M', time)
+    else
+        time = os.date('%b %d  %Y', time)
+    end
+
+    local size = self._file:size()
+    return string.format('%s %s', size and ya.readable_size(size) or '-', time)
+end
+
+-- Bookmarks
 local home = os.getenv('HOME')
 
 require('bookmarks'):setup({
@@ -50,17 +69,3 @@ require('bookmarks'):setup({
     },
     path = (os.getenv('XDG_STATE_HOME') or home .. '/.local/state') .. '/yazi/bookmarks',
 })
-
-function Linemode:size_and_mtime()
-    local time = math.floor(self._file.cha.mtime or 0)
-    if time == 0 then
-        time = ''
-    elseif os.date('%Y', time) == os.date('%Y') then
-        time = os.date('%b %d %H:%M', time)
-    else
-        time = os.date('%b %d  %Y', time)
-    end
-
-    local size = self._file:size()
-    return string.format('%s %s', size and ya.readable_size(size) or '-', time)
-end
