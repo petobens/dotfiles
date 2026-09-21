3ad7af21fca4b3e2faef1b5611a4cef3058b87cc
petobens <ferrari_pedro@yahoo.com>
2026-09-21 09:31:29 -0300

    chore(surfingkeys): replace AWS shortcut with Appsmith

--- config/home/surfingkeysrc.js
+++ config/home/surfingkeysrc.js
@@ -142,7 +142,7 @@ const qmarksMapKey = function (prefix, urls) {
 };
 
 const qmarksUrls = {
-    a: 'https://aws.amazon.com/console',
+    a: 'http://appsmith.muttdata.ai/',
     b: 'www.bing.com/chat',
     c: 'www.utdt.edu/campusvirtual',
     d: 'https://drive.google.com/drive/u/0/folders/0B9ulz1YH9ei7dGJValg1Tm9tMVE',
