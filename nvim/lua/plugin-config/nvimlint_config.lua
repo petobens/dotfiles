e55fa2e8453c0ab9b5af6c2fb40ffdc8fcc31ed2
petobens <ferrari_pedro@yahoo.com>
2026-09-24 22:55:53 -0300

    chore(nvim): remove redundant SQLFluff parser wrapper
    Use nvim-lint's existing diagnostic codes instead of copying them again
    from user_data. Use the local linters alias consistently.

--- nvim/lua/plugin-config/nvimlint_config.lua
+++ nvim/lua/plugin-config/nvimlint_config.lua
@@ -224,16 +224,8 @@ linters.zmypy = {
     end,
 }
 ---- Sqlfluff
-lint.linters.sqlfluff.args = { 'lint', '--format=json', '-' }
+linters.sqlfluff.args = { 'lint', '--format=json', '-' }
 linters.sqlfluff.stdin = true
-local fluff_parser = linters.sqlfluff.parser
-linters.sqlfluff.parser = function(output, bufnr)
-    local diagnostics = fluff_parser(output, bufnr)
-    for _, v in pairs(diagnostics) do
-        v.code = v.user_data.lsp.code
-    end
-    return diagnostics
-end
 ---- TeX
 linters.chktex.args = vim.list_extend(vim.deepcopy(linters.chktex.args), {
     '-n1',
