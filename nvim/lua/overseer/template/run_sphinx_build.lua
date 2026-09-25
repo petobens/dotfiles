5df7f983d404557cc4e3015071c773ccc2d41807
petobens <ferrari_pedro@yahoo.com>
2026-09-24 22:55:03 -0300

    fix(nvim): restrict Sphinx build tasks to suitable projects
    Generate the task only when the project has pyproject.toml and
    docs/Makefile, avoiding unusable tasks in unrelated Markdown and Python
    projects. Preserve the existing build command and error formatting.

--- nvim/lua/overseer/template/run_sphinx_build.lua
+++ nvim/lua/overseer/template/run_sphinx_build.lua
@@ -1,29 +1,42 @@
 return {
-    name = 'run_sphinx_build',
-    builder = function()
-        local pyvenv = (_G.PyVenv and _G.PyVenv.active_venv) or {}
-        local package_manager = pyvenv.package_manager or 'uv'
-        local project_root = vim.fs.root(0, 'pyproject.toml')
+    generator = function(opts)
+        local project_root = vim.fs.root(opts.dir, 'pyproject.toml')
+        if not project_root then
+            return {}
+        end
         local docs_dir = vim.fs.joinpath(project_root, 'docs')
+        local makefile = vim.uv.fs_stat(vim.fs.joinpath(docs_dir, 'Makefile'))
+        if not makefile or makefile.type ~= 'file' then
+            return {}
+        end
         return {
-            cwd = docs_dir,
-            cmd = { package_manager, 'run', 'make', 'html' },
-            components = {
-                { 'on_complete_notify', statuses = { 'SUCCESS' } },
-                { 'on_output_quickfix', open_on_match = true, close = true },
-                'default',
-            },
-            default_component_params = {
-                errorformat = ''
-                    .. [[%E%f:%l: SEVER%t: %m,]]
-                    .. [[%f:%l: %tRROR: %m,]]
-                    .. [[%f:%l: %tARNING: %m,]]
-                    .. [[%E%f:: SEVER%t: %m,]]
-                    .. [[%f:: %tRROR: %m,]]
-                    .. [[%f:: %tARNING: %m,]]
-                    .. [[%trror: %m,]]
-                    .. [[%+W%.%#:%m\ (%f,\ line\ %l),]]
-                    .. [[%-G%.%#,]],
+            {
+                name = 'run_sphinx_build',
+                builder = function()
+                    local pyvenv = (_G.PyVenv and _G.PyVenv.active_venv) or {}
+                    local package_manager = pyvenv.package_manager or 'uv'
+                    return {
+                        cwd = docs_dir,
+                        cmd = { package_manager, 'run', 'make', 'html' },
+                        components = {
+                            { 'on_complete_notify', statuses = { 'SUCCESS' } },
+                            { 'on_output_quickfix', open_on_match = true, close = true },
+                            'default',
+                        },
+                        default_component_params = {
+                            errorformat = ''
+                                .. [[%E%f:%l: SEVER%t: %m,]]
+                                .. [[%f:%l: %tRROR: %m,]]
+                                .. [[%f:%l: %tARNING: %m,]]
+                                .. [[%E%f:: SEVER%t: %m,]]
+                                .. [[%f:: %tRROR: %m,]]
+                                .. [[%f:: %tARNING: %m,]]
+                                .. [[%trror: %m,]]
+                                .. [[%+W%.%#:%m\ (%f,\ line\ %l),]]
+                                .. [[%-G%.%#,]],
+                        },
+                    }
+                end,
             },
         }
     end,
